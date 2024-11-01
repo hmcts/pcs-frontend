02af4bbcc1795bf9562dac52e69bf0710b9c3fb3
MoLala1 <Mohammed.lala1@hmcts.net>
2024-11-01 13:29:40 +0000

    removed connection string from secrets

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -3,6 +3,6 @@ import { Application } from 'express';
 
 export default function (app: Application): void {
   app.get('/', (req, res) => {
-    res.render('home', { connectionString: config.get('secrets.pcs.redis-connection-string') });
+    res.render('home', { connectionString: config.get('pcs.redis-connection-string') });
   });
 }
