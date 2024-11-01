8d07da4464ff9f685f3c9e6d914e708cc7b5b2b7
MoLala1 <Mohammed.lala1@hmcts.net>
2024-11-01 12:55:50 +0000

    added test redis

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,7 +1,8 @@
+import config from 'config';
 import { Application } from 'express';
 
 export default function (app: Application): void {
   app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { connectionString: config.get('secrets.pcs.redis-connection-string') });
   });
 }
