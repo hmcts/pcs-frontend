6aeae8737320eff40bfd0c2ad228dfadc41a577e
MoLala1 <Mohammed.lala1@hmcts.net>
2024-11-04 17:55:55 +0000

    try/catch for redis connection

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -3,10 +3,26 @@ import { Application } from 'express';
 import { Redis } from 'ioredis';
 
 export default async function (app: Application): Promise<void> {
-  const redis = new Redis(config.get('pcs.redis-connection-string'));
-  await redis.set('mykey', 'value');
   app.get('/', async (req, res) => {
-    const mykey = await redis.get('mykey');
-    res.render('home', { redisKey: mykey });
+    let logMsg = '';
+    if (!app.locals.redis) {
+      try {
+        app.locals.redis = new Redis(config.get('pcs.redis-connection-string'), {
+          maxRetriesPerRequest: 3,
+        });
+        await app.locals.redis.set('mykey', 'value');
+      } catch (error) {
+        logMsg = `${error.message} - Unable to connect and Set to redis`;
+        app.locals.redis.disconnect();
+      }
+    }
+
+    try {
+      const val = await app.locals.redis.get('mykey');
+      logMsg += ` - value: ${val}`;
+    } catch (err) {
+      logMsg += ` ${err.message} - unable to get my key`;
+    }
+    res.render('home', { redisKey: logMsg });
   });
 }
