0b964042afc85e180c424a0b7d66149ba1cf0738
MoLala1 <Mohammed.lala1@hmcts.net>
2024-11-04 12:49:15 +0000

    added test for redis connection

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,8 +1,12 @@
 import config from 'config';
 import { Application } from 'express';
+import { Redis } from 'ioredis';
 
 export default function (app: Application): void {
-  app.get('/', (req, res) => {
-    res.render('home', { connectionString: config.get('pcs.redis-connection-string') });
+  const redis = new Redis(config.get('pcs.redis-connection-string'));
+  redis.set('mykey', 'value');
+  app.get('/', async (req, res) => {
+    const mykey = await redis.get('mykey');
+    res.render('home', { connectionString: mykey });
   });
 }
