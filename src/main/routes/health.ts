5dd4654fa1cdca87afdfc3413d1264c395e48ff1
MoLala1 <Mohammed.lala1@hmcts.net>
2024-11-11 18:23:54 +0000

    accidental deletion of healthcheck

---
+++ src/main/routes/health.ts
@@ -0,0 +1,29 @@
+import { app as myApp } from '../app';
+
+import { Application } from 'express';
+
+const healthcheck = require('@hmcts/nodejs-healthcheck');
+
+function shutdownCheck(): boolean {
+  return myApp.locals.shutdown;
+}
+
+export default function (app: Application): void {
+  const healthCheckConfig = {
+    checks: {
+      redis: healthcheck.raw(() => {
+        if (app.locals.redisClient) {
+          return app.locals.redisClient.ping() ? healthcheck.up() : healthcheck.down();
+        }
+        return healthcheck.up();
+      }),
+    },
+    readinessChecks: {
+      shutdownCheck: healthcheck.raw(() => {
+        return shutdownCheck() ? healthcheck.down() : healthcheck.up();
+      }),
+    },
+  };
+
+  healthcheck.addTo(app, healthCheckConfig);
+}
