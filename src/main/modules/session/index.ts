bf461f33495159e1c14e3b4b60f3791029e1cd41
MoLala1 <Mohammed.lala1@hmcts.net>
2024-11-13 11:20:10 +0000

    set trust proxy and changed session cookie settings

--- src/main/modules/session/index.ts
+++ src/main/modules/session/index.ts
@@ -6,16 +6,6 @@ import { Redis } from 'ioredis';
 
 export class Session {
   enableFor(app: Express): void {
-    const sessionMiddleware: session.SessionOptions = {
-      secret: 'PCS-SECRET', //TODO: replace this
-      resave: false,
-      saveUninitialized: true,
-      cookie: { sameSite: 'strict' },
-      name: config.get('session.cookieName'),
-    };
-
-    console.log('conn str = ', config.get('secrets.pcs.redis-connection-string'), process.env.REDIS_CLOUD_URL);
-
     const redis = new Redis(config.get('secrets.pcs.redis-connection-string'));
     redis.on('error', (err: typeof Error) => console.error('REDIS ERROR', err));
     app.locals.redisClient = redis;
@@ -26,12 +16,20 @@ export class Session {
       prefix: config.get('session.prefix') + ':',
       ttl: config.get('session.ttlInSeconds'),
     });
-    sessionMiddleware.store = redisStore;
 
-    if (config.get<string>('node-env').toLowerCase() === 'production' && sessionMiddleware.cookie) {
-      sessionMiddleware.cookie.secure = true; // serve secure cookies
-    }
+    const sessionMiddleware: session.SessionOptions = {
+      secret: 'PCS-SECRET', //TODO: replace this
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: 'strict',
+        secure: config.get<string>('node-env').toLowerCase() === 'production',
+      },
+      name: config.get('session.cookieName'),
+      store: redisStore,
+    };
 
+    app.set('trust proxy', true);
     app.use(session(sessionMiddleware));
   }
 }
