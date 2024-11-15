c010f3bbf059244b04d9654fd24d8ecde4c746fa
MoLala1 <Mohammed.lala1@hmcts.net>
2024-11-15 11:14:48 +0000

    fix

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -4,7 +4,7 @@ const appInsights = require('applicationinsights');
 
 export class AppInsights {
   enable(): void {
-    if (config.has<string>('secrets.pcs.app-insights-connection-string')) {
+    if (config.get<string>('secrets.pcs.app-insights-connection-string')) {
       appInsights
         .setup(config.get<string>('secrets.pcs.app-insights-connection-string'))
         .setSendLiveMetrics(true)
