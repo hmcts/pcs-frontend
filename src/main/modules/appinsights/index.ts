341bdf5d5c0b1d3ab6cc74964911fe370d389858
Jake Gowler <jakecgowler@gmail.com>
2024-11-15 15:16:15 +0000

    Attempt #3

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -7,7 +7,7 @@ export class AppInsights {
     if (config.get<string>('secrets.pcs.app-insights-connection-string')) {
       appInsights.setup(config.get<string>('secrets.pcs.app-insights-connection-string')).setSendLiveMetrics(true);
 
-      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'pcs-frontend';
+      appInsights.defaultClient.context.tags['ai.cloud.role'] = 'pcs-frontend';
       appInsights.defaultClient.trackTrace({
         message: 'App insights activated',
       });
