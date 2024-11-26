bdb9a7613101458d1f92efc219a00f17ed4a909d
Jake Gowler <jakecgowler@gmail.com>
2024-11-26 10:20:10 +0000

    HDPI-88 - Assigning cloud role before starting appInsights

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -5,12 +5,10 @@ const appInsights = require('applicationinsights');
 export class AppInsights {
   enable(): void {
     if (config.get<string>('secrets.pcs.app-insights-connection-string')) {
-      appInsights
-        .setup(config.get<string>('secrets.pcs.app-insights-connection-string'))
-        .setSendLiveMetrics(true)
-        .start();
+      appInsights.setup(config.get<string>('secrets.pcs.app-insights-connection-string')).setSendLiveMetrics(true);
 
       appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'pcs-frontend';
+      appInsights.start();
       appInsights.defaultClient.trackTrace({
         message: 'App insights activated',
       });
