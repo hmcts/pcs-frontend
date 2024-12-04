d5234022ae2a9e77a69e0288faca49e6e7745226
Jake Gowler <jakecgowler@gmail.com>
2024-12-04 10:45:58 +0000

    HDPI-89 - Linting fix

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,19 +1,19 @@
 // import { Application, type Request, type Response } from 'express';
 
-// declare module 'express-session' {
-//   export interface SessionData {
-//     views: number;
-//   }
-// }
+declare module 'express-session' {
+  export interface SessionData {
+    views: number;
+  }
+}
 
 // export default function (app: Application): void {
-  // app.get('/', (req: Request, res: Response) => {
-    // if (req.session.views) {
-    //   req.session.views++;
-    // } else {
-    //   req.session.views = 1;
-    // }
-    //
-    // res.render('home', { views: req.session.views });
-  // });
+// app.get('/', (req: Request, res: Response) => {
+// if (req.session.views) {
+//   req.session.views++;
+// } else {
+//   req.session.views = 1;
+// }
+//
+// res.render('home', { views: req.session.views });
+// });
 // }
