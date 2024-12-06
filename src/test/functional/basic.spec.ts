77ab718d103fd66dbe5acc6806dc89c2c04c0802
JakeCG <jakecgowler@gmail.com>
2024-12-06 14:51:19 +0000

    HDPI-96 - Adding accessibility test helper (#126)
    * HDPI-96 - Adding accessibility test helper

--- src/test/functional/basic.spec.ts
+++ src/test/functional/basic.spec.ts
@@ -2,7 +2,7 @@ import { config as testConfig } from '../config';
 
 import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title @accessibility', async ({ page }) => {
   await page.goto(testConfig.TEST_URL);
 
   await expect(page.locator('.govuk-heading-xl')).toHaveText('Welcome to the PCS home page');
