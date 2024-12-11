cc3f909c0be0a8b88049a60ac05b02769937f67d
Jake Gowler <jakecgowler@gmail.com>
2024-12-11 12:04:39 +0000

    HDPI-100 - Updating README.md for testing

--- src/test/config.ts
+++ src/test/config.ts
@@ -5,25 +5,12 @@ process.on('unhandledRejection', reason => {
 
 export const config = {
   TEST_URL: process.env.TEST_URL || 'http://localhost:3209',
-  TestHeadlessBrowser: process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true,
-  TestSlowMo: 250,
-  WaitForTimeout: 10000,
 
-  Gherkin: {
-    features: './src/test/functional/features/**/*.feature',
-    steps: ['./src/test/steps/common.ts'],
-  },
   helpers: {},
 };
 
 config.helpers = {
   Playwright: {
     url: config.TEST_URL,
-    show: !config.TestHeadlessBrowser,
-    browser: 'chromium',
-    waitForTimeout: config.WaitForTimeout,
-    waitForAction: 1000,
-    waitForNavigation: 'networkidle0',
-    ignoreHTTPSErrors: true,
   },
 };
