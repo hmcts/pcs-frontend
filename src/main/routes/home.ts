81b3a571424a40fc5c43a6513d2113a36da5f477
MoLala1 <Mohammed.lala1@hmcts.net>
2024-12-13 12:28:57 +0000

    lint fixes

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -2,13 +2,6 @@ import axios from 'axios';
 import config from 'config';
 import { Application, Request, Response } from 'express';
 
-
-declare module 'express-session' {
-  export interface SessionData {
-    serviceToken: string;
-  }
-}
-
 export default function (app: Application): void {
   app.get('/', async (req: Request, res: Response) => {
     const apiResponse = await axios.get(config.get('api.url'));
--- src/test/functional/basic.spec.ts
+++ src/test/functional/basic.spec.ts
@@ -1,6 +1,6 @@
-import { config as testConfig } from '../config';
-
 import { expect, test } from '@playwright/test';
+
+import { config as testConfig } from '../config';
 
 test('has title @accessibility @PR @nightly', async ({ page }) => {
   await page.goto(testConfig.TEST_URL);
