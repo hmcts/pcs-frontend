782785f3f1311717c559418a69d69fd2f0505a69
MoLala1 <Mohammed.lala1@hmcts.net>
2024-12-16 13:24:53 +0000

    restructured s2s totp

--- src/main/modules/s2s/index.ts
+++ src/main/modules/s2s/index.ts
@@ -18,27 +18,28 @@ export class S2S {
     const s2sSecret = config.get<string>('secrets.pcs.pcs-frontend-s2s-secret');
     const s2sUrl = config.get('s2s.url');
 
-    const oneTimePassword = new otp({ secret: s2sSecret }).totp(0);
-    this.logger.info('S2S oneTimePassword', oneTimePassword);
     this.logger.info('s2sUrl', s2sUrl);
     this.logger.info('s2sSecret', s2sSecret);
     this.logger.info('microservice', microservice);
 
-    const options = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json;charset=UTF-8',
-      },
-      body: JSON.stringify({
-        microservice,
-        oneTimePassword,
-      }),
-    };
-
     app.use(async (req: Request, res: Response, next: NextFunction) => {
       if (!req.session.serviceToken) {
         try {
+          const oneTimePassword = new otp({ secret: s2sSecret }).totp(0);
+          const options = {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json;charset=UTF-8',
+            },
+            body: JSON.stringify({
+              microservice,
+              oneTimePassword,
+            }),
+          };
+
+          this.logger.info('S2S oneTimePassword', oneTimePassword);
+
           const response = await fetch(`${s2sUrl}/lease`, options);
           req.session.serviceToken = await response.json();
         } catch (error) {
