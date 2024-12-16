88e847cb24d7a7142f45064d5d4532ea779d8e18
MoLala1 <Mohammed.lala1@hmcts.net>
2024-12-16 18:11:03 +0000

    test new method

--- src/main/modules/s2s/index.ts
+++ src/main/modules/s2s/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import config from 'config';
 import { Express, NextFunction, Request, Response } from 'express';
-import otp from 'otp';
+import { TOTP } from 'totp-generator';
 
 declare module 'express-session' {
   export interface SessionData {
@@ -22,27 +22,71 @@ export class S2S {
     this.logger.info('s2sSecret', s2sSecret);
     this.logger.info('microservice', microservice);
 
+
     app.use(async (req: Request, res: Response, next: NextFunction) => {
-      if (!req.session.serviceToken) {
-        try {
-          const oneTimePassword = new otp({ secret: s2sSecret }).totp(0);
-          this.logger.info('S2S oneTimePassword', oneTimePassword);
 
-          const response = await axios.post(`${s2sUrl}/lease`, {
-            microservice,
-            oneTimePassword,
+      const { otp } = TOTP.generate(s2sSecret);
+
+      const params = {
+        microservice,
+        oneTimePassword: otp,
+      };
+
+      const data = await new Promise((resolve, reject) => {
+        fetch(
+          `${s2sUrl}/lease`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(params),
+          },
+        )
+          .then((res: any) => res.text())
+          .then((token: string) => {
+            resolve(token);
+          })
+          .catch((err: string) => {
+            this.logger.error(err);
+            reject(err);
           });
-          this.logger.info('S2S request status: ', response.status, response.statusText, response.data);
-          req.session.serviceToken = response.data;
-        } catch (error) {
-          this.logger.error('S2S ERROR', error.response.data, error.message);
-        }
+      });
+
+      if (data) {
+        this.logger.info('SERVICE TOKEN = ', s2sUrl, data);
+        axios.defaults.headers.common['ServiceAuthorization'] = `Bearer ${data}`;
       }
 
-      if (req.session.serviceToken) {
-        this.logger.info('SERVICE TOKEN = ', s2sUrl, req.session.serviceToken);
-        axios.defaults.headers.common['ServiceAuthorization'] = `Bearer ${req.session.serviceToken}`;
-      }
+      // let serviceToken = '';
+
+      // // if (!req.session.serviceToken) {
+      //   try {
+      //     const { otp } = TOTP.generate(s2sSecret);
+      //     this.logger.info('S2S oneTimePassword', otp);
+
+
+
+
+
+
+
+
+      //     const response = await axios.post(`${s2sUrl}/lease`, {
+      //       microservice,
+      //       oneTimePassword: otp,
+      //     });
+      //     this.logger.info('S2S request status: ', response.status, response.statusText, response.data);
+      //     serviceToken = response.data;
+      //   } catch (error) {
+      //     this.logger.error('S2S ERROR', error.response.data, error.message);
+      //   }
+      // // }
+
+      // // if (req.session.serviceToken) {
+      //   this.logger.info('SERVICE TOKEN = ', s2sUrl, serviceToken);
+      //   axios.defaults.headers.common['ServiceAuthorization'] = `Bearer ${serviceToken}`;
+      // // }
       next();
     });
   }
