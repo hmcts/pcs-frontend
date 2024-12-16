67b58dba330aa93fb4f4a11347db9a930ab308fe
MoLala1 <Mohammed.lala1@hmcts.net>
2024-12-16 12:38:25 +0000

    added error handling

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,10 +1,18 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import config from 'config';
 import { Application, Request, Response } from 'express';
 
 export default function (app: Application): void {
   app.get('/', async (req: Request, res: Response) => {
-    const apiResponse = await axios.get(config.get('api.url'));
+    let apiResponse: Partial<AxiosResponse> = {
+      data: 'default value',
+    };
+    try {
+      apiResponse = await axios.get(config.get('api.url'));
+    } catch (error) {
+      console.error('pcs-api error', error.response.statusText);
+    }
+    
     res.render('home', { apiResponse: apiResponse.data });
   });
 }
