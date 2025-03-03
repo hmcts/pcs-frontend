7fadedde06f9235e23b3c519e3bc7581f2140b3e
Peter Pilgrim <peter.pilgrim@gmail.com>
2025-03-03 10:56:02 +0000

    HDPI-143 IDAM authentication for PCS frontend - initial effort copied over template from Civil Examplar

---
+++ src/main/app/controller/AppRequest.ts
@@ -0,0 +1,63 @@
+import type { LoggerInstance } from 'winston';
+
+import { Request } from 'express';
+import { Session } from 'express-session';
+
+
+// import { Case, CaseWithId } from '../case/case';
+// import { FormError } from '../form/Form';
+
+export interface AppRequest<T = Partial<Case>> extends Request {
+  session: AppSession;
+  locals: {
+    env: string;
+    lang: string;
+    logger: LoggerInstance;
+  };
+  body: T;
+}
+
+export interface AppSession extends Session {
+  submissionError: boolean;
+  user: UserDetails;
+  userCase: CaseWithId;
+  userCaseList: CaseWithId[];
+  eligibility: Eligibility;
+  lang: string | undefined;
+  errors: FormError[] | undefined;
+  addresses: [];
+  fileErrors: anyErrors[];
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  returnUrl?: string;
+  accessCodeLoginIn: boolean;
+  c100RebuildLdFlag: boolean;
+  isDataVerified: boolean;
+  loggedInSystemUserType: string;
+}
+export interface UserDetails {
+  accessToken: string;
+  id: string;
+  email: string;
+  givenName: string;
+  familyName: string;
+  roles: string[];
+}
+
+export interface Eligibility {
+  under18Eligible?: string;
+  marriedEligible?: string;
+  livedUKEligible?: string;
+  under21Eligible?: string;
+}
+
+export interface DocumentRequest {
+  id: string;
+  value: {
+    document: {
+      document_url: string;
+      document_binary_url: string;
+      document_filename: string;
+    };
+    comment: string;
+  };
+}
---
+++ src/main/routes/authless/landing/get_test.ts
@@ -0,0 +1,55 @@
+import { mockRequest } from '../../../../test/unit/mocks/mockRequest';
+import { mockResponse } from '../../../../test/unit/mocks/mockResponse';
+
+import { LandingGetController } from './get';
+
+describe('Test URL endpoints', () => {
+  const controller = new LandingGetController();
+
+  const res = mockResponse();
+  const req = mockRequest();
+
+  test('should redirect user to case finder page if already authenticated with IDAM', async () => {
+    await controller.get(req, res);
+    expect(res.redirect).toHaveBeenCalledWith(CASE_SEARCH_URL);
+  });
+
+  test('should redirect user to landing page if user not authenticated with IDAM', async () => {
+    delete req.session.user;
+    await controller.get(req, res);
+    expect(res.render).toHaveBeenCalledWith('landing.njk', expect.any(Object));
+  });
+
+  test('should set ToggleLanguage to cy', async () => {
+    delete req.session.user;
+    req.query.lang = 'en';
+    await controller.get(req, res);
+    expect(res.render).toHaveBeenCalledWith('landing.njk', {
+      loginURL: SIGN_IN_URL,
+      content: expect.any(Object),
+      ToggleLanguage: 'cy',
+    });
+  });
+
+  test('should set ToggleLanguage to en', async () => {
+    delete req.session.user;
+    req.query.lang = 'cy';
+    await controller.get(req, res);
+    expect(res.render).toHaveBeenCalledWith('landing.njk', {
+      loginURL: SIGN_IN_URL,
+      content: expect.any(Object),
+      ToggleLanguage: 'en',
+    });
+  });
+
+  test('should set ToggleLanguage to default', async () => {
+    delete req.session.user;
+    req.query.lang = '';
+    await controller.get(req, res);
+    expect(res.render).toHaveBeenCalledWith('landing.njk', {
+      loginURL: SIGN_IN_URL,
+      content: expect.any(Object),
+      ToggleLanguage: 'cy',
+    });
+  });
+});
