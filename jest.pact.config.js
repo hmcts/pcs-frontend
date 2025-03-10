0145e73d4e73b136f5b750d82eae57ed7dde878b
marianadpereira <mariana.pereira@HMCTS.NET>
2025-03-10 18:33:34 +0000

    fixing typo

--- jest.pact.config.js
+++ jest.pact.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  roots: ['<rootDir>/src/test/contact/consumer'],
-  testRegex: '(/src/test/contact/consumer/*|\\.test)\\.(ts|js)$',
+  roots: ['<rootDir>/src/test/contract/consumer'],
+  testRegex: '(/src/test/contract/consumer/*|\\.test)\\.(ts|js)$',
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts$': 'ts-jest',
