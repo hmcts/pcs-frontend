043e8034e147b50ae541c7dc7a65323c1517995d
sstewart <scott.stewart@solirius.com>
2025-04-23 11:50:43 +0100

    HDPI-612: Basic templating of the dashboard notifications
    This is WIP

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -1,6 +1,8 @@
 import * as path from 'path';
 
 import * as express from 'express';
+import { DateTime } from 'luxon';
+import { Environment } from 'nunjucks';
 import * as nunjucks from 'nunjucks';
 
 export class Nunjucks {
@@ -16,9 +18,33 @@ export class Nunjucks {
       express: app,
     });
 
+    this.addCustomFilters(app.locals.nunjucksEnv);
+
     app.use((req, res, next) => {
       res.locals.pagePath = req.path;
       next();
     });
   }
+
+  private addCustomFilters(nunjucksEnv: Environment) {
+    nunjucksEnv.addFilter('dateWithTime', function(isoDateTime: string) {
+      return DateTime.fromISO(isoDateTime)
+        .setZone('Europe/London')
+        .setLocale('en-gb')
+        .toFormat('d LLLL y \'at\' h:mma');
+    });
+
+    nunjucksEnv.addFilter('date', function(isoDate: string) {
+      return DateTime.fromISO(isoDate)
+        .setZone('Europe/London')
+        .setLocale('en-gb')
+        .toFormat('d LLLL y');
+    });
+
+    nunjucksEnv.addFilter('gbp', function(amount: number) {
+      return '£' + amount.toFixed(2);
+    });
+
+  }
+
 }
