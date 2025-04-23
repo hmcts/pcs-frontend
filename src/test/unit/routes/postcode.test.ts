cfd92b2dc5c0052eb5fb3fece6d7a7d32a8744b0
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2025-04-23 13:19:24 +0100

    HDPI-515: fixing the CodeQL warning

--- src/test/unit/routes/postcode.test.ts
+++ src/test/unit/routes/postcode.test.ts
@@ -31,7 +31,7 @@ describe('POST /postcode', () => {
         secret: 'test-secret',
         resave: false,
         saveUninitialized: true,
-        cookie: { secure: false, httpOnly: true }, // test-safe settings
+        cookie: { secure: process.env.NODE_ENV !== 'test', httpOnly: true }
       })
     );
 
