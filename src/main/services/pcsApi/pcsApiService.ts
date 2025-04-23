358f48c4cf259c43569a03250dc1f440cde5857a
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2025-04-23 15:56:24 +0100

    HDPI-515: adding logging for the tester

--- src/main/services/pcsApi/pcsApiService.ts
+++ src/main/services/pcsApi/pcsApiService.ts
@@ -3,6 +3,9 @@ import config from 'config';
 
 import type { CourtVenue } from '../../interface/courtVenue.interface';
 
+const { Logger } = require('@hmcts/nodejs-logging');
+const logger = Logger.getLogger('pcsApiService');
+
 function getBaseUrl(): string {
   return config.get('api.url');
 }
@@ -14,6 +17,9 @@ export const getRootGreeting = async (): Promise<string> => {
 };
 
 export const getCourtVenues = async (postcode: string): Promise<CourtVenue[]> => {
-  const response = await axios.get(`${getBaseUrl()}/courts?postcode=${encodeURIComponent(postcode)}`);
+  const url = `${getBaseUrl()}/courts?postcode=${encodeURIComponent(postcode)}`;
+  logger.info(`Calling PCS court search with URL: ${url}`);
+  const response = await axios.get(url);
+  logger.debug(`Court venue response for ${postcode}: ${JSON.stringify(response.data)}`);
   return response.data as CourtVenue[];
 };
