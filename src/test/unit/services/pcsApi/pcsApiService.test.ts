bed4093706e9f745711def11b6ff48eba9e52405
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2025-05-01 09:12:14 +0100

    HDPI-500: fixing merge conflict and issues

--- src/test/unit/services/pcsApi/pcsApiService.test.ts
+++ src/test/unit/services/pcsApi/pcsApiService.test.ts
@@ -31,23 +31,27 @@ test('should fetch root greeting', () => {
     expect(axios.get).toHaveBeenCalledWith(testApiBase);
   });
 });
-
-test('should fetch court venues by postcode', () => {
+test('should fetch court venues by postcode', async () => {
   const expectedCourtVenues: CourtVenue[] = [
     {
-      epimmsId: 101,
-      courtVenueId: 1001,
-      courtName: 'some name',
+      epimId: 101,
+      id: 1001,
+      name: 'some name',
     },
   ];
 
   stubAxiosGet(expectedCourtVenues);
 
   const postcode: string = 'PC12 3AQ';
+  const mockAccessToken = 'test-token';
 
-  return getCourtVenues(postcode).then((actualCourtVenues: CourtVenue[]) => {
-    expect(actualCourtVenues).toEqual(expectedCourtVenues);
-    expect(axios.get).toHaveBeenCalledWith(`${testApiBase}/courts?postCode=${encodeURIComponent(postcode)}`);
+  const actualCourtVenues = await getCourtVenues(postcode, { accessToken: mockAccessToken });
+
+  expect(actualCourtVenues).toEqual(expectedCourtVenues);
+  expect(axios.get).toHaveBeenCalledWith(`${testApiBase}/courts?postcode=${encodeURIComponent(postcode)}`, {
+    headers: {
+      Authorization: `Bearer ${mockAccessToken}`,
+    },
   });
 });
 
