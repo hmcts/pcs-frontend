52b3476631627a2e2c5681d0cca091bfa0c2238f
Pardeep Singh Basi <pardeep.basi@hmcts.net>
2025-05-13 19:48:50 +0100

    test: updated route and unit tests

--- src/main/routes/dashboard.ts
+++ src/main/routes/dashboard.ts
@@ -3,7 +3,6 @@ import { Application, Request, Response } from 'express';
 
 import { oidcMiddleware } from '../middleware/oidc';
 import {
-  type DashboardNotification,
   type DashboardTaskGroup,
   STATUS_MAP,
   TASK_GROUP_MAP,
@@ -11,60 +10,81 @@ import {
   getDashboardTaskGroups,
 } from '../services/pcsApi';
 
+interface MappedTask {
+  title: { html: string };
+  hint?: { html: string };
+  href?: string;
+  status: {
+    text: string;
+    tag: { text: string; classes: string };
+  };
+}
+
+interface MappedTaskGroup {
+  groupId: DashboardTaskGroup['groupId'];
+  title: string;
+  tasks: MappedTask[];
+}
+
+const mapTaskGroups =
+  (app: Application, caseReference: number) =>
+  (taskGroups: DashboardTaskGroup[]): MappedTaskGroup[] => {
+    return taskGroups.map(taskGroup => {
+      const mappedTitle = TASK_GROUP_MAP[taskGroup.groupId];
+      return {
+        groupId: taskGroup.groupId,
+        title: mappedTitle,
+        tasks: taskGroup.tasks.map(task => {
+          if (!app.locals.nunjucksEnv) {
+            throw new Error('Nunjucks environment not initialized');
+          }
+
+          const tag = STATUS_MAP[task.status];
+          const taskGroupId = taskGroup.groupId.toLowerCase();
+
+          const hint =
+            task.templateValues.dueDate || task.templateValues.deadline
+              ? {
+                  html: app.locals.nunjucksEnv.render(
+                    `components/taskGroup/${taskGroupId}/${task.templateId}-hint.njk`,
+                    task.templateValues
+                  ),
+                }
+              : undefined;
+
+          return {
+            title: {
+              html: app.locals.nunjucksEnv.render(
+                `components/taskGroup/${taskGroupId}/${task.templateId}.njk`,
+                task.templateValues
+              ),
+            },
+            hint,
+            // TODO: slugify the templateId
+            href: task.status === 'NOT_AVAILABLE' ? undefined : `${caseReference}/${taskGroupId}/${task.templateId}`,
+            status: {
+              text: task.status,
+              tag,
+            },
+          };
+        }),
+      };
+    });
+  };
+
 export default function (app: Application): void {
   const logger = Logger.getLogger('dashboard');
+
   app.get('/dashboard/:caseReference', oidcMiddleware, async (req: Request, res: Response) => {
     const caseReference: number = parseInt(req.params.caseReference, 10);
     try {
-      const data: [DashboardNotification[], DashboardTaskGroup[]] = await Promise.all([
+      const [notifications, taskGroups] = await Promise.all([
         getDashboardNotifications(caseReference),
-        getDashboardTaskGroups(caseReference),
+        getDashboardTaskGroups(caseReference).then(mapTaskGroups(app, caseReference)),
       ]);
 
-      const taskGroups = data[1].map(taskGroup => {
-        const mappedTitle = TASK_GROUP_MAP[taskGroup.groupId];
-        return {
-          ...taskGroup,
-          title: mappedTitle,
-          tasks: taskGroup.tasks.map(task => {
-            if (!app.locals.nunjucksEnv) {
-              throw new Error('Nunjucks environment not initialized');
-            }
-
-            const tag = STATUS_MAP[task.status];
-            const taskGroupId = taskGroup.groupId.toLowerCase();
-
-            const hint =
-              task.templateValues.dueDate || task.templateValues.deadline
-                ? {
-                    html: app.locals.nunjucksEnv.render(
-                      `components/taskGroup/${taskGroupId}/${task.templateId}-hint.njk`,
-                      task.templateValues
-                    ),
-                  }
-                : undefined;
-
-            return {
-              title: {
-                html: app.locals.nunjucksEnv.render(
-                  `components/taskGroup/${taskGroupId}/${task.templateId}.njk`,
-                  task.templateValues
-                ),
-              },
-              hint,
-              // TODO: slugify the templateId
-              href: task.status === 'NOT_AVAILABLE' ? undefined : `${caseReference}/${taskGroupId}/${task.templateId}`,
-              status: {
-                text: task.status,
-                tag,
-              },
-            };
-          }),
-        };
-      });
-
       res.render('dashboard', {
-        notifications: data[0],
+        notifications,
         taskGroups,
       });
     } catch (e) {
