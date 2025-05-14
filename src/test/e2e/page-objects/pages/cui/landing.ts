ee39b677027087e7f0b4aae7c364752c03b69562
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-05-14 14:38:38 +0100

    Idam user login

--- src/test/e2e/page-objects/pages/cui/landing.ts
+++ src/test/e2e/page-objects/pages/cui/landing.ts
@@ -1,8 +1,9 @@
-import {Page} from "@playwright/test";
-import {Base} from "../../base";
+import { Page } from '@playwright/test';
+
+import { Base } from '../../base';
 
 export class LandingPage extends Base {
-  readonly heading = this.page.locator("govuk-heading-xl");
+  readonly heading = this.page.locator('govuk-heading-xl');
 
   constructor(page: Page) {
     super(page);
