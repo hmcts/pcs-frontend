373f5fe2d5c5e945e4555b0afadc54f7b0a67ad5
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-05-19 15:50:41 +0100

    update test configuration to use dynamic URL and change client ID

--- src/test/functional/basic.spec.ts
+++ src/test/functional/basic.spec.ts
@@ -1,18 +1,17 @@
 import { expect, test } from '@playwright/test';
 import config from 'config';
 
-import { config as testConfig } from '../config';
 import { buildUserDataWithRole } from '../e2e/TestConfig';
 import * as idamHelper from '../e2e/helpers/userHelpers/IdamHelper';
 import { LandingPage } from '../e2e/page-objects/pages/cui/landing';
 import { IdamPage } from '../e2e/page-objects/pages/idam.login';
 
 test.skip('has title @accessibility @PR @nightly', async ({ page }) => {
-  await page.goto(testConfig.TEST_URL);
+  await page.goto(config.get('e2e.testURL'));
   await expect(page.locator('.govuk-heading-xl')).toHaveText('Welcome to the PCS home page');
 });
 test('login to application', async ({ page }) => {
-  await page.goto(testConfig.TEST_URL);
+  await page.goto(config.get('e2e.testURL'));
   const password = config.get<string>('secrets.pcs.pcs-frontend-idam-user-temp-password');
   const userData = buildUserDataWithRole('citizen', password);
   await idamHelper.deleteAccount(userData.user.email);
