93142c15a2e7540b842ee47d2832f2e70e5edf1c
elvianihuseyin2 <elviani.huseyin2@hmcts.net>
2025-05-23 10:46:40 +0100

    Hdpi 698 customer journey step navigation 2 (#379)
    Co-authored-by: “elvianihuseyin2” <“elviani.huseyin2@hmcts.net”>

---
+++ src/main/interfaces/stepFormData.interface.ts
@@ -0,0 +1,27 @@
+import { RequestHandler } from 'express';
+
+import { GetController } from '../app/controller/GetController';
+
+export interface StepFormData {
+  title?: string;
+  error?: string;
+  answer?: string;
+  choices?: string[] | string;
+  serviceName?: string;
+  buttons?: {
+    continue?: string;
+    back?: string;
+    cancel?: string;
+  };
+  [key: string]: unknown;
+}
+
+export interface StepDefinition {
+  url: string;
+  name: string;
+  view: string;
+  stepDir: string;
+  generateContent: () => StepFormData;
+  getController: GetController;
+  postController?: { post: RequestHandler };
+}
