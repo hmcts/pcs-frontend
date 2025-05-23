fe510a8c793b748d8ea520406566d3d7ab0d51a6
elvianihuseyin2 <elviani.huseyin2@hmcts.net>
2025-05-23 10:46:40 +0100

    Hdpi 698 customer journey step navigation 2 (#379)
    Co-authored-by: “elvianihuseyin2” <“elviani.huseyin2@hmcts.net”>

---
+++ src/test/unit/app/controller/sessionHelper.test.ts
@@ -0,0 +1,33 @@
+import { Request } from 'express';
+
+import { getFormData, setFormData } from '../../../../main/app/controller/sessionHelper';
+
+describe('sessionHelper', () => {
+  it('should return empty object if no formData is present', () => {
+    const req = { session: {} } as unknown as Request;
+    expect(getFormData(req, 'step1')).toEqual({});
+  });
+  it('should retrieve form data for a given step', () => {
+    const req = {
+      session: {
+        formData: {
+          step1: { answer: 'Yes' },
+        },
+      },
+    } as unknown as Request;
+
+    expect(getFormData(req, 'step1')).toEqual({ answer: 'Yes' });
+  });
+
+  it('should set form data for a given step', () => {
+    const req = {
+      session: {
+        formData: {},
+      },
+    } as unknown as Request;
+
+    setFormData(req, 'step2', { answer: 'No' });
+
+    expect(req.session.formData?.step2).toEqual({ answer: 'No' });
+  });
+});
