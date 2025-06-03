dd51b5423b94662b2673de7ffffc2633d186c1c2
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-06-03 12:25:34 +0100

    Pact tests disabled and moved home.ts to unit tests

--- src/test/unit/routes/home.test.ts
+++ src/test/unit/routes/home.test.ts
@@ -1,15 +1,14 @@
-import { expect } from 'chai';
 import request from 'supertest';
 
 import { app } from '../../../main/app';
 
-/* eslint-disable jest/expect-expect */
+jest.mock('../../../main/modules/oidc/oidc');
+
 describe('Home page', () => {
   describe('on GET', () => {
-    test('should return sample home page', async () => {
-      await request(app)
-        .get('/')
-        .expect(res => expect(res.status).to.equal(200));
+    it('should return sample home page', async () => {
+      const response = await request(app).get('/');
+      expect(response.status).toBe(302);
     });
   });
 });
