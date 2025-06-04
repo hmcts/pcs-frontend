f065aba94d892373302ac18e85ceed8978ed67e9
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-06-04 09:30:58 +0100

    Updated test folder structure and UI tests and (#394)
    Co-authored-by: Jake Gowler <jakecgowler@gmail.com>

---
+++ src/test/ui/common/data/index.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+const constants = require('./constants.data.json');
+const testData = require('./testData.data.json');
+
+module.exports = { testData, constants };
---
+++ src/test/ui/common/helpers/element-helpers/index.ts
@@ -0,0 +1,1 @@
+export * from './webElements.helper';
