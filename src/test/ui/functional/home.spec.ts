14761dacadb8d74ac44a439492022ced58e33b1f
lovelin <lovelin.marcus@hmcts.net>
2025-06-26 13:44:32 +0100

    Initial commit for configuring an allure report. Removed the artifact storage as the report generates a screenshot and video on the left side folder.

--- src/test/ui/functional/home.spec.ts
+++ src/test/ui/functional/home.spec.ts
@@ -1,4 +1,5 @@
 import { test } from '@playwright/test';
+import * as allure from 'allure-js-commons';
 import config from 'config';
 
 import { loginHelper } from '../common/helpers';
@@ -10,6 +11,7 @@ const test_url = (process.env.TEST_URL as string) || config.get('e2e.testUrl');
 
 test.beforeEach(async ({ page }) => {
   initActionHelper(page);
+  await allure.parentSuite('Home Page');
 });
 
 test('Idam Login @accessibility @PR @nightly', async ({ page }) => {
