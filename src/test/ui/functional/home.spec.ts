0dbd844ac8569781be0247ca145eb9d771d388e3
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-06-26 10:01:58 +0100

    Dashboard notifications and tasklist verifications

--- src/test/ui/functional/home.spec.ts
+++ src/test/ui/functional/home.spec.ts
@@ -2,18 +2,37 @@ import { test } from '@playwright/test';
 import config from 'config';
 
 import { loginHelper } from '../common/helpers';
-import { initActionHelper, performAction } from '../common/helpers/element-helpers';
-
-const { constants } = require('../common/data');
+import { initActionHelper } from '../common/helpers/element-helpers';
+import { initVerificationHelper, performVerification } from '../common/helpers/element-helpers/verification.helper';
 
 const test_url = (process.env.TEST_URL as string) || config.get('e2e.testUrl');
 
 test.beforeEach(async ({ page }) => {
   initActionHelper(page);
-});
-
-test('Idam Login @accessibility @PR @nightly', async ({ page }) => {
+  initVerificationHelper(page);
   await page.goto(test_url);
   await loginHelper.login(page);
-  await performAction('verifyPageTitle', constants.homePage.title);
+});
+
+test('Idam Login @accessibility @PR @nightly', async () => {
+  await performVerification('verifyPageTitle', 'GOV.UK - The best place to find government services and information');
+});
+
+test('Dashboard Notifications @PR @nightly', async ({ page }) => {
+  const dashboardURL = test_url + '/dashboard/1';
+  await page.goto(dashboardURL);
+
+  await performVerification(
+    'dashboardNotification',
+    'Trial or hearing scheduled',
+    'Your appointment is on 20 May 2025 at 11:30am in London. You need to pay £76.00 by 20 May 2025. View the hearing notice.'
+  );
+  await performVerification('taskListItem', 'verify-text-link', 'Upload hearing documents', '');
+  await performVerification('taskListItem', 'verify-status', 'Upload hearing documents', 'Action needed');
+  await performVerification(
+    'taskListItem',
+    'verify-deadline',
+    'Upload hearing documents',
+    'Deadline is 4:00 pm on 20 May 2025'
+  );
 });
