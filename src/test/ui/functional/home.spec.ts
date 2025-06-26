9b52ee8354a4fe72ea6526cdedd2cdd175b74e04
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2025-06-26 12:06:46 +0100

    Dashboard notifications and tasklist verifications

--- src/test/ui/functional/home.spec.ts
+++ src/test/ui/functional/home.spec.ts
@@ -12,27 +12,30 @@ test.beforeEach(async ({ page }) => {
   initVerificationHelper(page);
   await page.goto(test_url);
   await loginHelper.login(page);
+  const dashboardURL = test_url + '/dashboard/1';
+  await page.goto(dashboardURL);
 });
 
 test('Idam Login @accessibility @PR @nightly', async () => {
   await performVerification('verifyPageTitle', 'GOV.UK - The best place to find government services and information');
 });
 
-test('Dashboard Notifications @PR @nightly', async ({ page }) => {
-  const dashboardURL = test_url + '/dashboard/1';
-  await page.goto(dashboardURL);
-
-  await performVerification(
-    'dashboardNotification',
-    'Trial or hearing scheduled',
-    'Your appointment is on 20 May 2025 at 11:30am in London. You need to pay £76.00 by 20 May 2025. View the hearing notice.'
-  );
-  await performVerification('taskListItem', 'verify-text-link', 'Upload hearing documents', '');
-  await performVerification('taskListItem', 'verify-status', 'Upload hearing documents', 'Action needed');
-  await performVerification(
-    'taskListItem',
-    'verify-deadline',
-    'Upload hearing documents',
-    'Deadline is 4:00 pm on 20 May 2025'
-  );
+test.describe('Verify Notifications and Tasks on Dashboard @PR @nightly', async () => {
+  test('Verify hearing notification', async () => {
+    await performVerification(
+      'dashboardNotification',
+      'Trial or hearing scheduled',
+      'Your appointment is on 20 May 2025 at 11:30am in London. You need to pay £76.00 by 20 May 2025. View the hearing notice.'
+    );
+  });
+  test('Verify Upload hearing documents task', async () => {
+    await performVerification('taskListItem', 'verify-text-link', 'Upload hearing documents', '');
+    await performVerification('taskListItem', 'verify-status', 'Upload hearing documents', 'Action needed');
+    await performVerification(
+      'taskListItem',
+      'verify-deadline',
+      'Upload hearing documents',
+      'Deadline is 4:00 pm on 20 May 2025'
+    );
+  });
 });
