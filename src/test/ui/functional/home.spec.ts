edcc5bb82b6e4ae0e214f4bfce13e916d93563a2
madhavi1203 <Madhavi.Atmakuri1@HMCTS.NET>
2025-06-27 16:55:16 +0100

    Dashboard Notifications and Tasklist helper methods added. (#450)
    HDPI-926: Dashboard Notifications and tasklists

--- src/test/ui/functional/home.spec.ts
+++ src/test/ui/functional/home.spec.ts
@@ -2,8 +2,10 @@ import { test } from '@playwright/test';
 import { parentSuite } from 'allure-js-commons';
 import config from 'config';
 
+import dashboard from '../common/data/dashboard';
 import { loginHelper } from '../common/helpers';
-import { initActionHelper, performAction } from '../common/helpers/element-helpers';
+import { initActionHelper } from '../common/helpers/element-helpers';
+import { initVerificationHelper, performVerification } from '../common/helpers/element-helpers/verification.helper';
 
 const { constants } = require('../common/data');
 
@@ -12,10 +14,29 @@ const test_url = (process.env.TEST_URL as string) || config.get('e2e.testUrl');
 test.beforeEach(async ({ page }) => {
   initActionHelper(page);
   await parentSuite('Home Page');
-});
-
-test('Idam Login @accessibility @PR @nightly', async ({ page }) => {
+  initVerificationHelper(page);
   await page.goto(test_url);
   await loginHelper.login(page);
-  await performAction('verifyPageTitle', constants.homePage.title);
+  const dashboardURL = test_url + '/dashboard/1';
+  await page.goto(dashboardURL);
+});
+
+test('Idam Login @accessibility @PR @nightly', async () => {
+  await performVerification('verifyPageTitle', constants.homePage.title);
+});
+test.describe('Verify Notifications and Tasks on Dashboard @PR @nightly', async () => {
+  test('Verify hearing notification', async () => {
+    const notification = dashboard.responseTimeElapsed('28');
+    await performVerification('dashboardNotification', notification.title, notification.content);
+  });
+  test('Verify Pay Hearing Fee task', async () => {
+    const payTheHearingFeeTaskList = dashboard.payTheHearingFee('28 June 2025', '4:00');
+    await performVerification(
+      'TaskListItem',
+      payTheHearingFeeTaskList.title,
+      'Action needed',
+      'true',
+      payTheHearingFeeTaskList.deadline
+    );
+  });
 });
