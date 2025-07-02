dca6872076c843ba484dd77520149113e2a98c1d
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-07-02 08:27:15 +0100

    Axe-Core integration to the e2e tests (#462)

--- src/test/ui/functional/home.spec.ts
+++ src/test/ui/functional/home.spec.ts
@@ -1,8 +1,8 @@
+import { AxeUtils } from '@hmcts/playwright-common';
 import { test } from '@playwright/test';
 import { parentSuite } from 'allure-js-commons';
 import config from 'config';
 
-import dashboard from '../common/data/dashboard';
 import { loginHelper } from '../common/helpers';
 import { initActionHelper } from '../common/helpers/element-helpers';
 import { initVerificationHelper, performVerification } from '../common/helpers/element-helpers/verification.helper';
@@ -17,26 +17,9 @@ test.beforeEach(async ({ page }) => {
   initVerificationHelper(page);
   await page.goto(test_url);
   await loginHelper.login(page);
-  const dashboardURL = test_url + '/dashboard/1';
-  await page.goto(dashboardURL);
 });
 
-test('Idam Login @accessibility @PR @nightly', async () => {
+test('Idam Login @accessibility @PR @nightly', async ({ page }) => {
   await performVerification('verifyPageTitle', constants.homePage.title);
-});
-test.describe('Verify Notifications and Tasks on Dashboard @PR @nightly', async () => {
-  test('Verify hearing notification', async () => {
-    const notification = dashboard.responseTimeElapsed('28');
-    await performVerification('dashboardNotification', notification.title, notification.content);
-  });
-  test('Verify Pay Hearing Fee task', async () => {
-    const payTheHearingFeeTaskList = dashboard.payTheHearingFee('28 June 2025', '4:00');
-    await performVerification(
-      'TaskListItem',
-      payTheHearingFeeTaskList.title,
-      'Action needed',
-      'true',
-      payTheHearingFeeTaskList.deadline
-    );
-  });
+  await new AxeUtils(page).audit();
 });
