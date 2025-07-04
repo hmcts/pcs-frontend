4fc8e89c4bbc9ab49689c5148e535988cae6cb2d
Pardeep Singh Basi <pardeep.basi@hmcts.net>
2025-07-04 19:01:51 +0100

    refactor: removed json in favour of ts DSL

---
+++ src/main/journeys/eligibility/steps/page4/step.ts
@@ -0,0 +1,36 @@
+import { StepDraft } from '../../../../modules/journey/engine/schema';
+
+const step: StepDraft = {
+  id: 'page4',
+  title: 'What is your address?',
+  type: 'form',
+  description: 'We need to know your address to send you a copy of the claim',
+  fields: {
+    address: {
+      type: 'text',
+      label: 'Address',
+    },
+    date: {
+      type: 'date',
+      label: 'Date of birth',
+      validate: {
+        errorMessages: {
+          day: 'Enter a valid day!!!!',
+          month: 'Enter a valid month!!!!',
+          year: 'Enter a valid year!!!!',
+          required: 'Enter your date of birth!!!!',
+          incomplete: 'Date of birth must include a day, month and year!!!!',
+          invalid: 'Date of birth must be a real date!!!!',
+          future: 'Date of birth must be in the past!!!!!',
+        },
+      },
+    },
+    postcode: {
+      type: 'text',
+      label: 'Postcode',
+    },
+  },
+  next: 'summary',
+};
+
+export default step; 
