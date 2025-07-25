f93a8b5ea4d7aa161318ac5dcd8eef8a7a29faab
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2025-07-25 14:08:50 +0100

    HDPI-699: update the language toggle

--- src/main/steps/page1/index.ts
+++ src/main/steps/page1/index.ts
@@ -1,15 +1,16 @@
 import { createGetController, createPostRedirectController } from '../../app/controller/controllerFactory';
+import { TranslationContent, loadTranslations } from '../../app/utils/loadTranslations';
 import type { StepDefinition } from '../../interfaces/stepFormData.interface';
+
+const generateContent = (lang = 'en'): TranslationContent => {
+  return loadTranslations(lang, ['common', 'page1']);
+};
 
 export const step: StepDefinition = {
   url: '/steps/page1',
   name: 'page1',
   view: 'steps/page1.njk',
-  generateContent: (lang = 'en') => {
-    const common = require(`../../../locales/${lang}/common.json`);
-    const page1Content = require(`../../../locales/${lang}/page1.json`);
-    return { ...common, ...page1Content };
-  },
+  generateContent,
   stepDir: __dirname,
   getController: (lang = 'en') => {
     const content = step.generateContent(lang);
