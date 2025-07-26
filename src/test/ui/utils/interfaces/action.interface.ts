ab393ae774782fa27aa29d08a1ccb6623525f538
Gautham059 <gautham.bhikkumalla@hmcts.net>
2025-07-27 00:23:31 +0100

    Updated interface to support returned values

--- src/test/ui/utils/interfaces/action.interface.ts
+++ src/test/ui/utils/interfaces/action.interface.ts
@@ -1,5 +1,5 @@
 import { Page } from '@playwright/test';
 
-export interface IAction {
-  execute(page: Page, fieldName: string, value?: string | number | boolean | string[] | object): Promise<void>;
+export interface IAction<T = void> {
+  execute(page: Page, fieldName: string, value?: string | number | boolean | string[] | object): Promise<T>;
 }
