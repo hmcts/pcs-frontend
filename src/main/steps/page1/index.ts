0e98f48579ce7b7d3169ebe4bfa9d834803e48b9
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2025-07-30 14:30:11 +0100

    HDPI-699: removing the original steps translation files

--- src/main/steps/page1/index.ts
+++ src/main/steps/page1/index.ts
@@ -1,20 +1,16 @@
 import { createGetController, createPostRedirectController } from '../../app/controller/controllerFactory';
-import { TranslationContent, loadTranslations } from '../../app/utils/loadTranslations';
+import common from '../../assets/locales/en/common.json';
+import page1Content from '../../assets/locales/en/page1.json';
 import type { StepDefinition } from '../../interfaces/stepFormData.interface';
 
-const generateContent = (lang = 'en'): TranslationContent => {
-  return loadTranslations(lang, ['common', 'page1']);
-};
+const content = { ...common, ...page1Content };
 
 export const step: StepDefinition = {
   url: '/steps/page1',
   name: 'page1',
   view: 'steps/page1.njk',
-  generateContent,
+  generateContent: () => content,
   stepDir: __dirname,
-  getController: (lang = 'en') => {
-    const content = step.generateContent(lang);
-    return createGetController('steps/page1.njk', 'page1', content);
-  },
+  getController: createGetController('steps/page1.njk', 'page1', content),
   postController: createPostRedirectController('/steps/page2'),
 };
