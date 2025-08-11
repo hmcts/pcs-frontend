4b38e8bc9740cebc06bac9070a5410247c5ff51e
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2025-08-11 16:36:46 +0100

    HDPI-1352: work in progress

--- src/main/journeys/eligibility/steps/page2/step.ts
+++ src/main/journeys/eligibility/steps/page2/step.ts
@@ -2,28 +2,31 @@ import { StepDraft } from '../../../../modules/journey/engine/schema';
 
 const step: StepDraft = {
   id: 'page2',
+  title: 'page2.title', // Reference translation key for the title
   type: 'form',
+  description: 'page2.description', // Reference translation key for description
   fields: {
     age: {
       type: 'radios',
       fieldset: {
         legend: {
-          text: 'Are you 18 or over?',
+          text: 'page2.ageLabel', // Translation key for label
           isPageHeading: true,
           classes: 'govuk-fieldset__legend--l',
         },
       },
       options: [
-        { value: 'yes', text: 'Yes' },
-        { value: 'no', text: 'No' },
+        { value: 'yes', text: 'page2.ageOptions.yes' }, // Translation key for option text
+        { value: 'no', text: 'page2.ageOptions.no' }, // Translation key for option text
       ],
       validate: {
         required: true,
-        customMessage: 'Select yes or no',
+        customMessage: 'page2.ageDescription', // Translation key for custom error message
       },
     },
     continueButton: {
       type: 'button',
+      text: 'buttons.continue', // Reference to the 'continue' translation key in common.json
       attributes: {
         type: 'submit',
       },
