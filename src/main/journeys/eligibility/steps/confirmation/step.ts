68e473fd388190d8acefe44f64e97c68bac76520
“elvianihuseyin2” <elviani.huseyin2@hmcts.net>
2025-08-12 15:33:03 +0100

    Adding confirmation page

--- src/main/journeys/eligibility/steps/confirmation/step.ts
+++ src/main/journeys/eligibility/steps/confirmation/step.ts
@@ -2,11 +2,11 @@ import { StepDraft } from '../../../../modules/journey/engine/schema';
 
 const step: StepDraft = {
   id: 'confirmation',
-  title: 'eligibility.confirmation.title',
+  title: 'confirmation.title',
   type: 'confirmation',
   data: {
     referenceNumber: true,
-    nextSteps: 'eligibility.confirmation.whatNext2',
+    nextSteps: 'confirmation.whatNext2',
   },
 };
 
