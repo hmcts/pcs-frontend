321fdedede6f7a4a7567e4e5d5d45988cde8110e
Jake Gowler <jakecgowler@gmail.com>
2025-08-13 11:21:39 +0100

    Fixing endpoint to be logging instead

--- src/main/routes/registerSteps.ts
+++ src/main/routes/registerSteps.ts
@@ -1,19 +1,34 @@
 import { Application } from 'express';
+import { Logger } from '@hmcts/nodejs-logging';
 
 import { ccdCaseMiddleware, oidcMiddleware } from '../middleware';
 import { protectedSteps, stepsWithContent } from '../steps';
 import { getValidatedLanguage } from '../utils/getValidatedLanguage';
 
+const logger = Logger.getLogger('registerSteps');
+
 export default function registerSteps(app: Application): void {
   for (const step of stepsWithContent) {
     const isProtected = protectedSteps.includes(step);
     const middlewares = isProtected ? [oidcMiddleware, ccdCaseMiddleware] : [];
-
     const allGetMiddleware = step.middleware ? [...middlewares, ...step.middleware] : middlewares;
 
     if (step.getController) {
       app.get(step.url, ...allGetMiddleware, (req, res) => {
         const lang = getValidatedLanguage(req);
+
+        logger.debug('Language information', {
+          url: req.url,
+          step: step.name,
+          validatedLang: lang,
+          reqLanguage: req.language,
+          langCookie: req.cookies?.lang,
+          langQuery: req.query?.lang,
+          headers: {
+            'accept-language': req.headers?.['accept-language'] || undefined
+          }
+        });
+
         const controller = typeof step.getController === 'function' ? step.getController(lang) : step.getController;
         return controller.get(req, res);
       });
@@ -22,9 +37,10 @@ export default function registerSteps(app: Application): void {
     if (step.postController?.post) {
       app.post(step.url, ...middlewares, step.postController.post);
     }
-
-    app.get('/lang-debug', (req, res) => {
-      res.send(`req.language = ${req.language}, lang cookie = ${req.cookies.lang}`);
-    });
   }
+
+  logger.info('Steps registered successfully', {
+    totalSteps: stepsWithContent.length,
+    protectedSteps: protectedSteps.length
+  });
 }
