ccae94b2acba8a766a24654fc7df71a47c866061
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-08-26 11:14:23 +0100

    Updated based on lynt fix

--- src/test/ui/utils/actions/element-actions/clickLink.action.ts
+++ src/test/ui/utils/actions/element-actions/clickLink.action.ts
@@ -1,11 +1,10 @@
-import { Locator, Page } from '@playwright/test';
+import { Page } from '@playwright/test';
 
 import { IAction } from '../../interfaces/action.interface';
 
 export class ClickLinkAction implements IAction {
   async execute(page: Page, fieldName: string): Promise<void> {
-    const locator: Locator = page.locator(`a:has-text("${fieldName}")`);
-    await locator.first().waitFor({ state: 'visible' });
-    await locator.first().click();
+    const locator = page.locator(`a:has-text("${fieldName}")`);
+    await locator.click();
   }
 }
--- src/test/ui/utils/validations/element-validations/pageHeader.validation.ts
+++ src/test/ui/utils/validations/element-validations/pageHeader.validation.ts
@@ -5,6 +5,6 @@ import { IValidation } from '../../interfaces/validation.interface';
 export class MainHeaderValidation implements IValidation {
   async validate(page: Page, fieldName: string): Promise<void> {
     const locator = page.locator('h1,h1.govuk-heading-xl, h1.govuk-heading-l');
-    await expect(locator).toHaveText(fieldName, { timeout: 10000 });
+    await expect(locator).toHaveText(fieldName);
   }
 }
