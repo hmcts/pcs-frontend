c9898febde40299c970c13bda634adbd6b14106b
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-08-26 10:59:29 +0100

    Updated based on lynt fix

--- src/test/ui/data/page-data/eligibility.page.data.ts
+++ src/test/ui/data/page-data/eligibility.page.data.ts
@@ -1,5 +1,5 @@
 export const eligibility = {
   title: 'GOV.UK - Eligibility Check',
   mainHeaderWelsh: 'cyBefore you start',
-  mainHeaderEnglish: 'Before you start'
+  mainHeaderEnglish: 'Before you start',
 };
