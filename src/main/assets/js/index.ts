f6d694b107e578425255c1dee113ccfd20fbc35c
Paddy Basi <pardeep.basi@hmcts.net>
2025-09-30 10:57:06 +0100

    Hdpi 1779 address capture (#615)

--- src/main/assets/js/index.ts
+++ src/main/assets/js/index.ts
@@ -1,7 +1,9 @@
 import '../scss/main.scss';
 import { initAll } from 'govuk-frontend';
 
+import { initPostcodeLookup } from './postcode-lookup';
 import { initPostcodeSelection } from './postcode-select';
 
 initAll();
 initPostcodeSelection();
+initPostcodeLookup();
---
+++ src/main/journeys/eligibility/steps/correspondenceAddress/step.ts
@@ -0,0 +1,74 @@
+import { StepDraft } from '../../../../modules/journey/engine/schema';
+
+const step: StepDraft = {
+  id: 'correspondenceAddress',
+  type: 'form',
+  title: 'Your correspondence address',
+  description:
+    'Your correspondence address is your postal address. Treetops Housing were asked to provide this so we could send you the claim documents by post.',
+  fields: {
+    isCorrespondenceAddress: {
+      type: 'radios',
+      fieldset: {
+        legend: {
+          text: 'Is this your correspondence address?',
+          classes: 'govuk-fieldset__legend--m',
+        },
+      },
+      items: [
+        {
+          value: 'yes',
+          text: 'Yes',
+        },
+        {
+          value: 'no',
+          text: 'No',
+          conditional: {
+            html: '',
+          },
+        },
+      ],
+      validate: {
+        required: true,
+        customMessage: 'Select yes or no',
+      },
+    },
+    correspondenceAddress: {
+      type: 'address',
+      label: {
+        text: 'Enter correspondence address',
+      },
+      lookupText: "What's your correspondence address?",
+      hint: {
+        text: 'This is the address we will send the claim documents to.',
+      },
+      validate: {
+        required: (stepData: Record<string, unknown>) => {
+          // Only require correspondence address if user selected "No" for isCorrespondenceAddress
+          return stepData.isCorrespondenceAddress === 'no';
+        },
+      },
+    },
+    continueButton: {
+      type: 'button',
+      text: 'Save and continue',
+      classes: 'govuk-button',
+      attributes: {
+        type: 'submit',
+      },
+    },
+    saveForLaterButton: {
+      type: 'button',
+      text: 'Save for later',
+      classes: 'govuk-button govuk-button--secondary',
+      attributes: {
+        type: 'submit',
+        name: '_saveForLater',
+        value: 'true',
+      },
+    },
+  },
+  next: 'summary',
+};
+
+export default step;
