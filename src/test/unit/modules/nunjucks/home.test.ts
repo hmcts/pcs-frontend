fce848a5089dd78da65e20ba9b670fffb5c62070
amandarichards <Amanda.Richards@HMCTS.NET>
2025-10-23 09:40:50 +0100

    Add homepage onboarding message

---
+++ src/test/unit/modules/nunjucks/home.test.ts
@@ -0,0 +1,23 @@
+import * as path from 'path';
+import * as nunjucks from 'nunjucks';
+import express from 'express';
+
+import { Nunjucks } from '../../../../main/modules/nunjucks';
+
+describe('home.njk renders personal greeting', () => {
+  let env: nunjucks.Environment;
+
+  beforeAll(() => {
+    const app = express();
+    const nunjucksInstance = new Nunjucks(false);
+    nunjucksInstance.enableFor(app);
+
+    const viewsPath = path.join(__dirname, '../../../../main/views');
+    env = nunjucks.configure(viewsPath, { autoescape: true });
+  });
+
+  it('renders "Hi from Amanda!"', () => {
+    const html = env.render('home.njk', { apiResponse: 'Test response' });
+    expect(html).toContain('<p>Hi from Amanda!</p>');
+  });
+});
