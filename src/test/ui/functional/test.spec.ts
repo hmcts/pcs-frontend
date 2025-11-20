4df8bdda50e0807e3123985a3daf4b6457ff99ad
Pardeep Singh Basi <pardeep.basi@hmcts.net>
2025-11-20 12:03:55 +0000

    fix: added dummy test for now

--- src/test/ui/functional/test.spec.ts
+++ src/test/ui/functional/test.spec.ts
@@ -1,7 +1,24 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
+import { parentSuite } from 'allure-js-commons';
+import config from 'config';
 
-// create a dummy test
-test('Dummy test', async ({ page }) => {
-  await page.goto('/');
-  expect(true).toBeTruthy();
+import { initializeExecutor, performAction, performValidation } from '../utils/controller';
+
+const baseUrl = (config.get('e2e.testUrl') as string);
+
+test.beforeEach(async ({ page }, testInfo) => {
+  await parentSuite('Home Page');
+  await testInfo.attach('Page URL', {
+    body: baseUrl,
+    contentType: 'text/plain',
+  });
+  initializeExecutor(page);
+  await performAction('createUserAndLogin', ['citizen']);
+});
+
+test.describe('Possession claims @PR @nightly', async () => {
+  test('Verify home page', async () => {
+    await performAction('navigateToUrl', baseUrl);
+    await performValidation('mainHeader', 'Home Page');
+  });
 });
