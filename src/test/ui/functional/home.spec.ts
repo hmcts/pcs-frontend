9532122282ede78474bc734a3b05a69bb22f2f5c
lovelin <lovelin.marcus@hmcts.net>
2025-11-25 18:07:47 +0000

    HDPI-3319-Align pcs-frontend with pcs-api to support CUI automation

--- src/test/ui/functional/home.spec.ts
+++ src/test/ui/functional/home.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
 import config from 'config';
 
-import { createCaseApiData, submitCaseApiData } from '../data/api-data';
+//import { createCaseApiData, submitCaseApiData } from '../data/api-data';
 import { home } from '../data/page-data';
 import { initializeExecutor, performAction, performValidation } from '../utils/controller';
 
@@ -9,8 +9,8 @@ const home_url = config.get('e2e.testUrl') as string;
 
 test.beforeEach(async ({ page }) => {
   initializeExecutor(page);
-  await performAction('createCaseAPI', { data: createCaseApiData.createCasePayload });
-  await performAction('submitCaseAPI', { data: submitCaseApiData.submitCasePayload });
+  //await performAction('createCaseAPI', { data: createCaseApiData.createCasePayload });
+  //await performAction('submitCaseAPI', { data: submitCaseApiData.submitCasePayload });
   await performAction('navigateToUrl', home_url);
   await performAction('createUserAndLogin', 'citizen', ['citizen']);
 });
