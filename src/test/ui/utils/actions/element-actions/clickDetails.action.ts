fe471fdf400a3ad7160963a3de085ca0a53e82bd
SameenaHMCTS <sameena.javeed@hmcts.net>
2025-12-10 22:39:26 +0000

    Test to validate page not found content

--- src/test/ui/utils/actions/element-actions/clickDetails.action.ts
+++ src/test/ui/utils/actions/element-actions/clickDetails.action.ts
@@ -4,9 +4,7 @@ import { IAction } from '../../interfaces';
 
 export class ClickDetailsAction implements IAction {
   async execute(page: Page, action: string, summaryText: string): Promise<void> {
-    const summary = page.locator(
-      `summary:has-text("${summaryText}")`
-    );
+    const summary = page.locator(`summary:has-text("${summaryText}")`);
     await summary.click();
   }
 }
