3bb435b1a24f76cd285cb8f9011dc9209706131e
amandarichards <Amanda.Richards@HMCTS.NET>
2026-01-12 14:43:42 +0000

    add contact preferences text message page

---
+++ src/main/steps/respond-to-claim/contact-preferences/index.ts
@@ -0,0 +1,36 @@
+import type { Request, Response } from 'express';
+
+import type { StepDefinition } from '../../../interfaces/stepFormData.interface';
+import { createGetController, createStepNavigation } from '../../../modules/steps';
+import { flowConfig } from '../flow.config';
+
+const stepName = 'contact-preferences';
+const stepNavigation = createStepNavigation(flowConfig);
+
+export const step: StepDefinition = {
+  url: '/respond-to-claim/contact-preferences',
+  name: stepName,
+  view: 'respond-to-claim/contact-preferences/contactPreferences.njk',
+  stepDir: __dirname,
+  getController: () => {
+    return createGetController(
+      'respond-to-claim/contact-preferences/contactPreferences.njk',
+      stepName,
+      undefined,
+      'respondToClaim'
+    );
+  },
+  postController: {
+    post: async (req: Request, res: Response) => {
+      // Get next step URL and redirect
+      const redirectPath = stepNavigation.getNextStepUrl(req, stepName, req.body);
+
+      if (!redirectPath) {
+        // No next step defined - show not found page
+        return res.status(404).render('not-found');
+      }
+
+      res.redirect(303, redirectPath);
+    },
+  },
+};
--- src/main/steps/respond-to-claim/flow.config.ts
+++ src/main/steps/respond-to-claim/flow.config.ts
@@ -3,7 +3,7 @@ import type { JourneyFlowConfig } from '../../interfaces/stepFlow.interface';
 export const flowConfig: JourneyFlowConfig = {
   basePath: '/respond-to-claim',
   journeyName: 'respondToClaim',
-  stepOrder: ['start-now', 'postcode-finder', 'free-legal-advice'],
+  stepOrder: ['start-now', 'postcode-finder', 'free-legal-advice', 'contact-preferences', 'contact-preferences-text-message'],
   steps: {
     'start-now': {
       defaultNext: 'postcode-finder',
@@ -14,5 +14,11 @@ export const flowConfig: JourneyFlowConfig = {
     'free-legal-advice': {
       defaultNext: 'legal-advice',
     },
+    'contact-preferences': {
+      defaultNext: 'contact-preferences-text-message',
+        },
+    'contact-preferences-text-message': {
+      defaultNext: '',
+         }
   },
 };
