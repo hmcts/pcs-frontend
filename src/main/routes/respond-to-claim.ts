08cb03fbcefc7db549111122c3b950f9b4e28076
MoLala1 <mohammed.lala1@hmcts.net>
2026-01-12 12:27:41 +0000

    HDPI-3339 Add Contact us for help accordion to Respond to a Claim pages

--- src/main/routes/respond-to-claim.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from 'express';
-
-const router = Router();
-
-router.get('/respond-to-claim/start-now', (req: Request, res: Response) => {
-  res.render('respond-to-claim/start-now');
-});
-
-export default router;
