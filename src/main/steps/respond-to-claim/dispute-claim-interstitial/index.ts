d3052f95fc28596563af73ee5e70a658f6eba449
Pardeep Singh Basi <pardeep.basi@hmcts.net>
2026-01-26 16:00:30 +0000

    feat: dispute claim page and claimant name interpolation

--- src/main/steps/respond-to-claim/dispute-claim-interstitial/index.ts
+++ src/main/steps/respond-to-claim/dispute-claim-interstitial/index.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from 'express';
 
 import type { StepDefinition } from '../../../interfaces/stepFormData.interface';
 import { createGetController, createStepNavigation } from '../../../modules/steps';
+import { DASHBOARD_ROUTE } from '../../../routes/dashboard';
 import { RESPOND_TO_CLAIM_ROUTE, flowConfig } from '../flow.config';
 
 const stepName = 'dispute-claim-interstitial';
@@ -17,8 +18,20 @@ export const step: StepDefinition = {
       'respond-to-claim/dispute-claim-interstitial/disputeClaimInterstitial.njk',
       stepName,
       async (req: Request) => {
+        // TODO:Retrieve claimantName dynamically from CCD case data and remove hardcoded default value
+        const claimantName = (req.session?.ccdCase?.data?.claimantName as string) || 'Treetops Housing';
+
+        const t = req.t;
+        if (!t) {
+          throw new Error('Translation function not available');
+        }
+
         return {
           backUrl: await stepNavigation.getBackUrl(req, stepName),
+          dashboardUrl: DASHBOARD_ROUTE,
+          // these keys override the translations from the step namespace but interpolate the claimantName
+          heading: t('heading', { claimantName }),
+          paragraph1: t('paragraph1', { claimantName }),
         };
       },
       'respondToClaim'
