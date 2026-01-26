de13bc8f3d5f2d5c2ec005cc64f11f7785c7a774
madhavi1203 <Madhavi.Atmakuri1@HMCTS.NET>
2026-01-26 19:42:20 +0000

    HDPI-3142-QA (#794)

--- src/test/ui/data/page-data/index.ts
+++ src/test/ui/data/page-data/index.ts
@@ -2,3 +2,4 @@ export * from './home.page.data';
 export * from './startNow.page.data';
 export * from './freeLegalAdvice.page.data';
 export * from './defendantNameCapture.page.data';
+export * from './defendantDateOfBirth.page.data';
--- src/test/ui/utils/actions/custom-actions/respondToClaim.action.ts
+++ src/test/ui/utils/actions/custom-actions/respondToClaim.action.ts
@@ -1,13 +1,15 @@
 import { Page } from '@playwright/test';
 
-import { freeLegalAdvice } from '../../../data/page-data';
-import { performAction } from '../../controller';
+import { defendantNameCapture, freeLegalAdvice } from '../../../data/page-data';
+import { performAction, performValidation } from '../../controller';
 import { IAction, actionData, actionRecord } from '../../interfaces';
 
 export class RespondToClaimAction implements IAction {
   async execute(page: Page, action: string, fieldName: actionData | actionRecord): Promise<void> {
     const actionsMap = new Map<string, () => Promise<void>>([
       ['selectLegalAdvice', () => this.selectLegalAdvice(fieldName)],
+      ['inputDefendantDetails', () => this.inputDefendantDetails(fieldName as actionRecord)],
+      ['inputErrorValidation', () => this.inputErrorValidation(fieldName as actionRecord)],
     ]);
     const actionToPerform = actionsMap.get(action);
     if (!actionToPerform) {
@@ -22,5 +24,53 @@ export class RespondToClaimAction implements IAction {
       option: legalAdviceData,
     });
     await performAction('clickButton', freeLegalAdvice.saveAndContinueButton);
+  }
+
+  private async inputDefendantDetails(defendantData: actionRecord): Promise<void> {
+    await performAction('inputText', defendantNameCapture.firstNameLabelText, defendantData.fName);
+    await performAction('inputText', defendantNameCapture.lastNameLabelText, defendantData.lName);
+    await performAction('clickButton', defendantNameCapture.saveAndContinueButton);
+  }
+
+  // Below changes are temporary will be changed as part of HDPI-3596
+  private async inputErrorValidation(validationArr: actionRecord) {
+    if (!validationArr || validationArr.validationReq !== 'YES') {
+      return;
+    }
+    if (!Array.isArray(validationArr.inputArray)) {
+      return;
+    }
+
+    for (const item of validationArr.inputArray) {
+      switch (validationArr.validationType) {
+        case 'radioOptions':
+          await performValidation(
+            'inputError',
+            !validationArr?.label ? validationArr.question : validationArr.label,
+            item.errMessage
+          );
+          if (validationArr.option) {
+            await performAction('clickRadioButton', { question: validationArr.question, option: validationArr.option });
+          }
+          break;
+
+        case 'textField':
+          await performValidation('inputError', item.label, item.errMessage);
+          await performValidation('errorMessage', { header: validationArr.header, message: item.errMessage });
+          break;
+
+        case 'checkBox':
+          await performAction('clickButton', validationArr.button);
+          await performValidation(
+            'inputError',
+            !validationArr?.label ? validationArr.question : validationArr.label,
+            item.errMessage
+          );
+          break;
+
+        default:
+          throw new Error(`Validation type :"${validationArr.validationType}" is not valid`);
+      }
+    }
   }
 }
