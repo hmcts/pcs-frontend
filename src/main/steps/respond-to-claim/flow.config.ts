13238284c54d6b3e84522a164c253c1f8e80d2ca
Alex Buzea <alex.buzea@hmcts.net>
2026-01-27 16:42:17 +0000

    initial commit wip page

--- src/main/steps/respond-to-claim/flow.config.ts
+++ src/main/steps/respond-to-claim/flow.config.ts
@@ -1,4 +1,4 @@
-import * as LDClient from '@launchdarkly/node-server-sdk';
+// import * as LDClient from '@launchdarkly/node-server-sdk';
 import { type Request } from 'express';
 
 import type { JourneyFlowConfig } from '../../interfaces/stepFlow.interface';
@@ -7,32 +7,34 @@ export const RESPOND_TO_CLAIM_ROUTE = '/case/:caseReference/respond-to-claim';
 
 //TODO need to add logic to check if defendant name is known from CCD case data
 const isDefendantNameKnown = async (req: Request): Promise<boolean> => {
-  const ldClient = (req.app?.locals?.launchDarklyClient as LDClient.LDClient | undefined) ?? undefined;
+  // const ldClient = (req.app?.locals?.launchDarklyClient as LDClient.LDClient | undefined) ?? undefined;
 
-  let result = '';
-  try {
-    const context: LDClient.LDContext = {
-      kind: 'user',
-      key: (req.session?.user?.uid as string) ?? 'anonymous',
-      name: req.session?.user?.name ?? 'anonymous',
-      email: req.session?.user?.email ?? 'anonymous',
-      firstName: req.session?.user?.given_name ?? 'anonymous',
-      lastName: req.session?.user?.family_name ?? 'anonymous',
-      custom: {
-        roles: req.session?.user?.roles ?? [],
-      },
-    };
+  // let result = '';
+  // try {
+  //   const context: LDClient.LDContext = {
+  //     kind: 'user',
+  //     key: (req.session?.user?.uid as string) ?? 'anonymous',
+  //     name: req.session?.user?.name ?? 'anonymous',
+  //     email: req.session?.user?.email ?? 'anonymous',
+  //     firstName: req.session?.user?.given_name ?? 'anonymous',
+  //     lastName: req.session?.user?.family_name ?? 'anonymous',
+  //     custom: {
+  //       roles: req.session?.user?.roles ?? [],
+  //     },
+  //   };
 
-    // If the flag does not exist LD will return the default (true) so UI remains visible by default.
-    result = await ldClient?.variation('defendant-name', context, '');
+  //   // If the flag does not exist LD will return the default (true) so UI remains visible by default.
+  //   result = await ldClient?.variation('defendant-name', context, '');
 
-    req.session.defendantName = result;
-  } catch (err: unknown) {
-    // eslint-disable-next-line no-console
-    console.error('LaunchDarkly evaluation failed', err);
-  }
+  //   req.session.defendantName = result;
+  // } catch (err: unknown) {
+  //   // eslint-disable-next-line no-console
+  //   console.error('LaunchDarkly evaluation failed', err);
+  // }
 
-  return Promise.resolve(result !== '');
+  // return Promise.resolve(result !== '');
+  req.session.defendantName = 'John Smith';
+  return Promise.resolve(true);
 };
 
 export const flowConfig: JourneyFlowConfig = {
@@ -72,7 +74,6 @@ export const flowConfig: JourneyFlowConfig = {
       defaultNext: 'defendant-date-of-birth',
     },
     'defendant-date-of-birth': {
-      previousStep: 'defendant-name-capture',
       defaultNext: 'postcode-finder',
     },
     'postcode-finder': {
