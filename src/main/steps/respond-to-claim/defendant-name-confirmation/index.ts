00c8b4945bba000901f383b9baab7faa5f98eebf
Alex Buzea <alex.buzea@hmcts.net>
2026-01-28 12:39:40 +0000

    removing logging and hardcoded value for routing

--- src/main/steps/respond-to-claim/defendant-name-confirmation/index.ts
+++ src/main/steps/respond-to-claim/defendant-name-confirmation/index.ts
@@ -18,11 +18,6 @@ export const step: StepDefinition = createFormStep(
       contactUs: 'contactUs',
     },
     extendGetContent: (req: Request) => {
-      console.log('=== defendant-name-confirmation extendGetContent ===');
-      console.log('Session exists:', !!req.session);
-      console.log('Session keys:', req.session ? Object.keys(req.session) : 'no session');
-      console.log('ccdCase in session:', req.session?.ccdCase);
-      console.log('Full session (stringified):', JSON.stringify(req.session, null, 2));
       //TODO: get defendant name from CCD case - currently served from LaunchDarkly flag
       const defendantName = req.session.defendantName ?? '';
       //TODO: get organisation name from CCD case - placeholder for now
