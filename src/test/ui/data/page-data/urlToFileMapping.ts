bdbf247c4af7ca9dc4e321cbd3ea5f05a5d3781e
madhavi1203 <Madhavi.Atmakuri1@HMCTS.NET>
2026-02-02 11:14:42 +0000

    Hdpi 3042-QA (#810)

--- src/test/ui/data/page-data/urlToFileMapping.ts
+++ src/test/ui/data/page-data/urlToFileMapping.ts
@@ -1,7 +1,8 @@
 export default {
   'start-now': 'startNow',
-  // 'free-legal-advice': 'freeLegalAdvice',
+  'free-legal-advice': 'freeLegalAdvice',
   home: 'home',
   'page-not-found': 'pageNotFound',
   'defendant-name-capture': 'defendantNameCapture',
+  'date-of-birth': 'dateOfBirth',
 };
