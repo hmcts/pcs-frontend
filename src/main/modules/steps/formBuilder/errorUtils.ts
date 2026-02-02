c9ff7bdda30be1653e1dd6a5f3aaf53976691427
Fahra Akhlaq <f.akhlaq@outlook.com>
2026-02-02 16:59:24 +0000

    HDPI-3043 (#799)
    Co-authored-by: Lovelin <lovelin.marcus@hmcts.net>

--- src/main/modules/steps/formBuilder/errorUtils.ts
+++ src/main/modules/steps/formBuilder/errorUtils.ts
@@ -123,6 +123,8 @@ export async function renderWithErrors(
   // res.render() sends the response directly and doesn't return a value
   res.status(400).render(viewPath, {
     ...formContent,
+    errors,
+    // errorSummary,
     stepName,
     journeyFolder,
     backUrl: await navigation.getBackUrl(req, stepName),
