6c51d77b05f489f26ee96256cf43239447b44dc1
ayub-macalim <ayub.macalim@hmcts.net>
2026-02-03 10:09:28 +0000

    fixed lint

--- src/main/steps/respond-to-claim/counter-claim/index.ts
+++ src/main/steps/respond-to-claim/counter-claim/index.ts
@@ -2,13 +2,11 @@ import type { StepDefinition } from '../../../interfaces/stepFormData.interface'
 import { createFormStep } from '../../../modules/steps';
 import { flowConfig } from '../flow.config';
 
-export const step: StepDefinition = createFormStep(
-  {
-    stepName: 'counter-claim',
-    journeyFolder: 'respondToClaim',
-    stepDir: __dirname,
-    flowConfig,
-    fields: [],
-    customTemplate: `${__dirname}/counterClaim.njk`,
-  }
-);
+export const step: StepDefinition = createFormStep({
+  stepName: 'counter-claim',
+  journeyFolder: 'respondToClaim',
+  stepDir: __dirname,
+  flowConfig,
+  fields: [],
+  customTemplate: `${__dirname}/counterClaim.njk`,
+});
