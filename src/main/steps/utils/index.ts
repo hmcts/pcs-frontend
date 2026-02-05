f2977e0396b7508a604012ecb621e11f937889ef
Liban Abdirahman <Liban.Abdirahman@HMCTS.NET>
2026-02-05 13:36:39 +0000

    HDPI-3478:Alligning routing with AC change

--- src/main/steps/utils/index.ts
+++ src/main/steps/utils/index.ts
@@ -3,3 +3,4 @@ export { isWelshProperty } from './isWelshProperty';
 export { isNoticeDateProvided } from './isNoticeDateProvided';
 export { isRentArrearsClaim } from './isRentArrearsClaim';
 export { isNoticeServed } from './isNoticeServed';
+export { getPreviousPageForArrears } from './journeyHelpers';
