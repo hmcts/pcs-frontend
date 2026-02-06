8714e036ca729fa8d6c3aa39a43aa4b7b758d551
Pardeep Singh Basi <pardeep.basi@hmcts.net>
2026-02-06 11:23:14 +0000

    feat: removed hardcoded value and fetching from case data

--- src/main/steps/respond-to-claim/dispute-claim-interstitial/index.ts
+++ src/main/steps/respond-to-claim/dispute-claim-interstitial/index.ts
@@ -18,18 +18,17 @@ export const step: StepDefinition = {
       'respond-to-claim/dispute-claim-interstitial/disputeClaimInterstitial.njk',
       stepName,
       async (req: Request) => {
-        // TODO:Retrieve claimantName dynamically from CCD case data and remove hardcoded default value
-        const claimantName = (req.session?.ccdCase?.data?.claimantName as string) || 'Treetops Housing';
-
         const t = req.t;
+
         if (!t) {
           throw new Error('Translation function not available');
         }
 
+        const claimantName = req.res?.locals.validatedCase.data.possessionClaimResponse.claimantOrganisations[0].value;
+
         return {
           backUrl: await stepNavigation.getBackUrl(req, stepName),
           dashboardUrl: DASHBOARD_ROUTE,
-          // these keys override the translations from the step namespace but interpolate the claimantName
           heading: t('heading', { claimantName }),
           paragraph1: t('paragraph1', { claimantName }),
         };
