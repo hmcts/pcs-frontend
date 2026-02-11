4c9fff3c00c0f7ca4171e47c2fefc869d756dd43
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-02-11 13:57:06 +0000

    HDPI-3549 Automation

---
+++ src/test/ui/data/page-data/nonRentArrearsDispute.page.data.ts
@@ -0,0 +1,9 @@
+export const nonRentArrearsDispute = {
+  mainHeader: 'Disputing other parts of the claim',
+  partOfClaimParagraph: 'You should view the claim (opens in new tab) to see if there’s any other parts of the claim that are incorrect or you disagree with. This includes: Treetops Housing’s grounds for possession (their reasons for making the claim) any documents they’ve uploaded to support their claim any other information they’ve given as part of their claim',
+  doYouWantToDisputeQuestion: 'Do you want to dispute any other parts of the claim?',
+  yesRadioOption: 'Yes',
+  noRadioOption: 'No',
+  saveForLaterButton: 'Save for later',
+  saveAndContinueButton: 'Save and continue',
+};
