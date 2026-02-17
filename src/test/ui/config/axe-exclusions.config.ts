f2253a5f0e6edc8fde9b0ee818e159fc9c455017
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-02-17 14:46:19 +0000

    axeutil failure fix

--- src/test/ui/config/axe-exclusions.config.ts
+++ src/test/ui/config/axe-exclusions.config.ts
@@ -1,5 +1,4 @@
 export const axe_exclusions = [
   '#correspondenceAddressConfirm-2', //page: correspondenceAddress, element: 'No', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
- '#confirmRepaymentsMade' //page: repaymentsMade, element: 'Yes', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
-
+  '#confirmRepaymentsMade', //page: repaymentsMade, element: 'Yes', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
 ];
