acf5aa37988998917b9c026ca86001a5ff24c924
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-02-23 18:19:26 +0000

    stage view

--- src/test/ui/functional/pageNotFound.spec.ts
+++ src/test/ui/functional/pageNotFound.spec.ts
@@ -21,6 +21,7 @@ test.afterEach(async () => {
 test.describe('Error page to indicate Page Not Found error @PR @nightly', () => {
   test('Content Validation on Page not found page', async () => {
     await performAction('navigateToUrl', home_url + '/page-not-found');
+    await performAction('login');
   });
 
   // This test was written as part of the story HDPI-3883. A new story will update the error message screen with a “Contact Us” link.
