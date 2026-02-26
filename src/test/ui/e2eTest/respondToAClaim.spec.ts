4a6f9d14f7e035828baca9c353b569aff7701202
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2026-02-26 11:36:00 +0000

    title fix

--- src/test/ui/e2eTest/respondToAClaim.spec.ts
+++ src/test/ui/e2eTest/respondToAClaim.spec.ts
@@ -48,7 +48,7 @@ test.afterEach(async () => {
 });
 
 test.describe('Respond to a claim - e2e Journey @nightly @PR', async () => {
-  test('Respond to a claim - Wales postcode @noDefendants', async () => {
+  test('Respond to a claim @noDefendants', async () => {
     await performAction('selectLegalAdvice', freeLegalAdvice.yesRadioOption);
     await performAction('inputDefendantDetails', {
       fName: defendantNameCapture.firstNameInputText,
