7b35c4f35a565fae42ca172f653abd242b897f24
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-03-03 09:58:52 +0000

    updated claimant name to read dynamic value

--- src/test/ui/data/page-data/nonRentArrearsDispute.page.data.ts
+++ src/test/ui/data/page-data/nonRentArrearsDispute.page.data.ts
@@ -1,3 +1,5 @@
+import { submitCaseApiData } from '../api-data';
+
 function generateRandomString(length: number): string {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
@@ -17,7 +19,7 @@ export const nonRentArrearsDispute = {
   viewTheClaimLink: 'view the claim (opens in new tab)',
   mainHeaderGovServiceHiddenNewTab: 'Welcome to GOV.UK',
   thisIncludesParagraph: 'This includes:',
-  groundsForPossessionList: 'Treetops Housing’s grounds for possession (their reasons for making the claim)',
+  groundsForPossessionList: `${submitCaseApiData.submitCasePayload.claimantName}’s grounds for possession (their reasons for making the claim)`,
   anyDocumentsList: 'any documents they’ve uploaded to support their claim',
   anyOtherList: ' any other information they’ve given as part of their claim',
   doYouWantToDisputeQuestion: 'Do you want to dispute any other parts of the claim?',
