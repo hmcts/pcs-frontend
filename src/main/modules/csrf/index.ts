894e51ef7731479da5237751e04ae37fe63311bc
Pardeep Singh Basi <pardeep.basi@hmcts.net>
2026-03-09 13:00:34 +0000

    feat: csrf

---
+++ src/main/modules/csrf/index.ts
@@ -0,0 +1,41 @@
+import { csrfSync } from 'csrf-sync';
+import { Express, NextFunction, Request, RequestHandler, Response } from 'express';
+
+import { Logger } from '@modules/logger';
+
+export class Csrf {
+  logger = Logger.getLogger('csrf');
+  enableFor(app: Express): void {
+    this.logger.info('Enabling CSRF protection');
+    const { csrfSynchronisedProtection }: { csrfSynchronisedProtection: RequestHandler } = csrfSync({
+      /**
+       * Extracts the CSRF token from the request body.
+       *
+       * @param {Request} req - The incoming request object.
+       * @returns {string|undefined} The CSRF token if present, otherwise undefined.
+       */
+      getTokenFromRequest: (req: Request) => req.body._csrf,
+    });
+
+    /**
+     * Middleware to enforce CSRF protection on incoming requests.
+     * This applies the `csrfSynchronisedProtection` middleware globally.
+     */
+    app.use(csrfSynchronisedProtection);
+
+    /**
+     * Middleware to expose the CSRF token to views.
+     * Adds `res.locals.csrfToken`, making it accessible in templates.
+     *
+     * @param {Request} req - The incoming request object.
+     * @param {Response} res - The response object.
+     * @param {NextFunction} next - Callback to pass control to the next middleware.
+     */
+    app.use((req: Request, res: Response, next: NextFunction) => {
+      if (typeof req.csrfToken === 'function') {
+        res.locals.csrfToken = req.csrfToken(); // Makes CSRF token available in views
+      }
+      next();
+    });
+  }
+}
