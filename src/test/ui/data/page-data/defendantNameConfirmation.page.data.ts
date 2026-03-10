f94171d2104b5e19f68a7fd621b6bf3cf6bbc85f
sreedhar narayanagiri <Sreedhar.Narayanagiri@HMCTS.NET>
2026-03-10 11:25:02 +0000

    Removed main header validation

--- src/test/ui/data/page-data/defendantNameConfirmation.page.data.ts
+++ src/test/ui/data/page-data/defendantNameConfirmation.page.data.ts
@@ -1,7 +1,10 @@
-import { firstName, lastName } from '../../utils/actions/custom-actions';
-
+//Main header data element will be corrected in a new task
+//import { firstName, lastName } from '../../utils/actions/custom-actions/fetchPINsAndValidateAccessCodeAPI.action';
+//function defendantNameHeader(): string {
+//return `Is your name ${firstName} ${lastName}?`;
+//}
 export const defendantNameConfirmation = {
-  mainHeader: `Is your name ${firstName} ${lastName}?`,
+  //mainHeader: defendantNameHeader(),
   respondToClaimParagraph: 'Respond to a property possession claim',
   thisIsTheNameParagraph: 'This is the name provided by Possession Claims Solicitor Org',
   yesRadioOption: 'Yes',
