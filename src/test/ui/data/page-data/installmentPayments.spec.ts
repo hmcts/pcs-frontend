0c7bd8ef1855ecd6766dbf4272be384c0979020a
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2026-03-11 17:13:33 +0000

    HDPI-3542-QA

---
+++ src/test/ui/data/page-data/installmentPayments.spec.ts
@@ -0,0 +1,4 @@
+export const installmentPayments = {
+  mainHeader: 'Non-Rent Arrears Dispute(Placeholder)',
+  continueButton: 'Continue',
+};
---
+++ src/test/ui/data/page-data/situationInterstitialScreen.page.data.ts
@@ -0,0 +1,4 @@
+export const situationInterstitialScreen = {
+  mainHeader: 'Non-Rent Arrears Dispute(Placeholder)',
+  continueButton: 'Continue',
+};
