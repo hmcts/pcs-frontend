d392613f60106dd48bf865568969e5871d075a59
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-03-12 08:40:39 +0000

    addressed review comments

--- src/test/ui/data/page-data/defendantNameConfirmation.page.data.ts
+++ src/test/ui/data/page-data/defendantNameConfirmation.page.data.ts
@@ -1,4 +1,5 @@
 import { firstName, lastName } from '../../utils/actions/custom-actions/fetchPINsAndValidateAccessCodeAPI.action';
+import { submitCaseApiData } from '../api-data';
 export const defendantNameConfirmation = {
   get mainHeader(): string {
     return `Is your name ${firstName} ${lastName}?`;
@@ -7,7 +8,7 @@ export const defendantNameConfirmation = {
     return `You must say if your name is ${firstName} ${lastName}`;
   },
   respondToClaimParagraph: 'Respond to a property possession claim',
-  thisIsTheNameHintText: 'This is the name provided by Possession Claims Solicitor Org',
+  thisIsTheNameHintText: `This is the name provided by ${submitCaseApiData.submitCasePayload.claimantName}`,
   yesRadioOption: 'Yes',
   noRadioOption: 'No',
   saveAndContinueButton: 'Save and continue',
