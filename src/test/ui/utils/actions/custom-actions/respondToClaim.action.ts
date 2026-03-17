5e254395c08171a8ad8c39f50c31e03bc350fd69
padmakumari <padma.kumari@hmcts.net>
2026-03-17 11:04:31 +0000

    fixed Lint Issues

--- src/test/ui/utils/actions/custom-actions/respondToClaim.action.ts
+++ src/test/ui/utils/actions/custom-actions/respondToClaim.action.ts
@@ -134,10 +134,9 @@ export class RespondToClaimAction implements IAction {
         contactPreferenceEmailOrPost.enterEmailAddressHiddenTextLabel,
         contactPreferenceData.emailAddress
       );
-      }
-      await performAction('clickButton', contactPreferenceEmailOrPost.saveAndContinueButton);
     }
-
+    await performAction('clickButton', contactPreferenceEmailOrPost.saveAndContinueButton);
+  }
 
   private async selectContactByTelephone(contactByPhoneData: actionRecord): Promise<void> {
     await performAction('clickRadioButton', {
