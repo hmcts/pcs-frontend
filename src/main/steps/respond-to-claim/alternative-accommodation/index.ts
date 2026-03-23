67c527185b4def824ec01db7a416986b8bb943d2
Liban Abdirahman <Liban.Abdirahman@HMCTS.NET>
2026-03-23 12:43:16 +0000

    HDPI-3569:Added alternative accommodation page

--- src/main/steps/respond-to-claim/alternative-accommodation/index.ts
+++ src/main/steps/respond-to-claim/alternative-accommodation/index.ts
@@ -1,13 +1,45 @@
 import type { StepDefinition } from '../../../interfaces/stepFormData.interface';
+import { createFormStep } from '../../../modules/steps';
 import { flowConfig } from '../flow.config';
 
-import { createFormStep } from '@modules/steps';
-
 export const step: StepDefinition = createFormStep({
-  stepName: 'would-you-have-somewhere-else-to-live-if-you-had-to-leave-your-home',
+  stepName: 'alternative-accommodation',
   journeyFolder: 'respondToClaim',
   stepDir: __dirname,
   flowConfig,
-  fields: [],
   customTemplate: `${__dirname}/alternativeAccommodation.njk`,
+  translationKeys: {
+    caption: 'caption',
+    question: 'question',
+  },
+  fields: [
+    {
+      name: 'confirmAlternativeAccommodation',
+      type: 'radio',
+      required: true,
+      translationKey: { label: 'question' },
+      legendClasses: 'govuk-fieldset__legend--l',
+      errorMessage: 'errors.confirmAlternativeAccommodation',
+      options: [
+        {
+          value: 'yes',
+          translationKey: 'options.yes',
+          subFields: {
+            alternativeAccommodationDate: {
+              name: 'alternativeAccommodationDate',
+              type: 'date',
+              required: false,
+              legendClasses: 'govuk-label--s govuk-!-font-weight-bold',
+              translationKey: {
+                label: 'dateLabel',
+              },
+            },
+          },
+        },
+        { value: 'no', translationKey: 'options.no' },
+        { divider: 'options.or' },
+        { value: 'notSure', translationKey: 'options.notSure' },
+      ],
+    },
+  ],
 });
