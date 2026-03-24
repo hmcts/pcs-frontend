814e7a4cec4dd0d93865451b0f6586dd2175b5f9
Nitu-Git-mac <nithya.ramakrishna@hmcts.net>
2026-03-24 13:31:49 +0000

    Updated page data file

--- src/test/ui/data/page-data/writtenTerms.page.data.ts
+++ src/test/ui/data/page-data/writtenTerms.page.data.ts
@@ -2,8 +2,7 @@ export const writtenTerms = {
   mainHeader: `Written Terms`,
   respondToClaimParagraph: `Respond to a property possession claim`,
   backLink: `Back`,
-  ItIsRequiredUnderTheRentingParagraph: `It is required under the Renting Homes Wales Act 2016 for a landlord to send you
-   written terms of the occupation contract.`,
+  itIsRequiredUnderTheRentingParagraph: `It is required under the Renting Homes Wales Act 2016 for a landlord to send you written terms of the occupation contract.`,
   hasYourLandlordSentYouWrittenTermsQuestion: `Has your landlord sent you written terms of the occupation contract?`,
   yesRadioOption: `Yes`,
   noRadioOption: `No`,
