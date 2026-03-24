103769b8066ec791fc02b4d302a6754755b55464
padmakumari <padma.kumari@hmcts.net>
2026-03-24 16:25:53 +0000

    updated start now pft file

--- src/test/ui/functional/startNow.pft.ts
+++ src/test/ui/functional/startNow.pft.ts
@@ -1,13 +1,10 @@
 import { dashboard, startNow } from '../data/page-data';
 import { performValidation } from '../utils/controller';
 
-
-
 export async function startNowNavigationTests(): Promise<void> {
   await performValidation('pageNavigation', startNow.feedbackLink, {
     element: startNow.feedbackParagraph,
     pageSlug: startNow.pageSlug,
   });
   await performValidation('pageNavigation', startNow.backLink, dashboard.mainHeader);
-
 }
