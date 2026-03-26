15839be81e5ca3903c18eff40e36801ccee3595b
lovelin <lovelin.marcus@hmcts.net>
2026-03-26 14:45:46 +0000

    addressed review comments

--- src/test/ui/utils/registry/action.registry.ts
+++ src/test/ui/utils/registry/action.registry.ts
@@ -15,20 +15,18 @@ import {
   InputTextAction,
   SelectAction,
   UploadFileAction,
-  clickLinkAndVerifyNewTabTitleAction,
-  clickLinkAndVerifySameTabTitleAction,
 } from '../actions/element-actions';
 import { ClickLinkAction } from '../actions/element-actions/clickLink.action';
 import { IAction } from '../interfaces';
 
 export class ActionRegistry {
-  private static actions: Map<string, IAction> = new Map([
+  private static actions: Map<string, IAction> = new Map<string, IAction>([
     ['check', new CheckAction()],
     ['clickButton', new ClickButtonAction()],
     ['clickSummary', new ClickSummaryAction()],
     ['clickLink', new ClickLinkAction()],
-    ['clickLinkAndVerifyNewTabTitle', new clickLinkAndVerifyNewTabTitleAction()],
-    ['clickLinkAndVerifySameTabTitle', new clickLinkAndVerifySameTabTitleAction()],
+    ['clickLinkAndVerifyNewTabTitle', new ClickLinkAction()],
+    ['clickLinkAndVerifySameTabTitle', new ClickLinkAction()],
     ['clickRadioButton', new ClickRadioButtonAction()],
     ['clickTab', new ClickTabAction()],
     ['inputText', new InputTextAction()],
