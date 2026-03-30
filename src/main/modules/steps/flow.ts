72e04728156b7db1f933e77a893dae36d150706a
sstewart <scott.stewart@solirius.com>
2026-03-30 13:55:06 +0100

    HDPI-5679: Use journey specific StepNavigation in flows
    This is to refactor out the hardcoded use of the RespondToClaim StepNavigation that was being used globally in a couple of places

--- src/main/modules/steps/flow.ts
+++ src/main/modules/steps/flow.ts
@@ -120,7 +120,7 @@ export function checkStepDependencies(
   return null;
 }
 
-export function createStepNavigation(flowConfig: JourneyFlowConfig): {
+export type StepNavigation = {
   getNextStepUrl: (
     req: Request,
     currentStepName: string,
@@ -128,7 +128,9 @@ export function createStepNavigation(flowConfig: JourneyFlowConfig): {
   ) => Promise<string | null>;
   getBackUrl: (req: Request, currentStepName: string) => Promise<string | null>;
   getStepUrl: (stepName: string, caseReference?: string) => string;
-} {
+};
+
+export function createStepNavigation(flowConfig: JourneyFlowConfig): StepNavigation {
   return {
     getNextStepUrl: async (
       req: Request,
@@ -153,8 +155,6 @@ export function createStepNavigation(flowConfig: JourneyFlowConfig): {
     },
   };
 }
-
-export const stepNavigation = createStepNavigation(respondToClaimFlowConfig);
 
 export function stepDependencyCheckMiddleware(flowConfig: JourneyFlowConfig = respondToClaimFlowConfig) {
   return (req: Request, res: Response, next: NextFunction): void => {
