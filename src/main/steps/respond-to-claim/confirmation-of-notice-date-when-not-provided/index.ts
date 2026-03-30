3d1f65247220d416e7fd614953266378d90dc0f7
Alex Buzea <alex.buzea@hmcts.net>
2026-03-30 15:30:53 +0100

    data persistence and pre population notice date unknown

--- src/main/steps/respond-to-claim/confirmation-of-notice-date-when-not-provided/index.ts
+++ src/main/steps/respond-to-claim/confirmation-of-notice-date-when-not-provided/index.ts
@@ -1,4 +1,10 @@
+import type { Request } from 'express';
+import { DateTime } from 'luxon';
+
+import type { CaseData, PossessionClaimResponse } from '../../../interfaces/ccdCase.interface';
 import type { StepDefinition } from '../../../interfaces/stepFormData.interface';
+import { formatDatePartsToISODate } from '../../utils/dateUtils';
+import { buildCcdCaseForPossessionClaimResponse } from '../../utils/populateResponseToClaimPayloadmap';
 import { flowConfig } from '../flow.config';
 
 import { createFormStep, getTranslationFunction } from '@modules/steps';
@@ -18,7 +24,7 @@ export const step: StepDefinition = createFormStep({
   },
   fields: [
     {
-      name: 'noticeDate',
+      name: 'noticeReceivedDate',
       type: 'date',
       required: false,
       noFutureDate: true,
@@ -30,6 +36,45 @@ export const step: StepDefinition = createFormStep({
       },
     },
   ],
+  getInitialFormData: req => {
+    const caseData: CaseData = req.res?.locals.validatedCase?.data;
+    const noticeReceivedDateRaw: string | undefined =
+      caseData?.possessionClaimResponse?.defendantResponses?.noticeReceivedDate;
+
+    if (!noticeReceivedDateRaw || typeof noticeReceivedDateRaw !== 'string') {
+      return {};
+    }
+
+    const dateTime: DateTime = DateTime.fromISO(noticeReceivedDateRaw);
+    if (!dateTime.isValid) {
+      return {};
+    }
+    console.log('dateTime', dateTime);
+
+    return {
+      noticeReceivedDate: {
+        day: dateTime.toFormat('dd'),
+        month: dateTime.toFormat('MM'),
+        year: dateTime.toFormat('yyyy'),
+      },
+    };
+  },
+
+  beforeRedirect: async (req: Request) => {
+    const dateObject: { day?: string; month?: string; year?: string } | undefined = req.body?.noticeReceivedDate;
+    const day = dateObject?.day !== undefined ? String(dateObject.day).trim() : '';
+    const month = dateObject?.month !== undefined ? String(dateObject.month).trim() : '';
+    const year = dateObject?.year !== undefined ? String(dateObject.year).trim() : '';
+    const noticeReceivedDate = formatDatePartsToISODate(day, month, year);
+
+    const possessionClaimResponse: PossessionClaimResponse = {
+      defendantResponses: {
+        ...(noticeReceivedDate && { noticeReceivedDate }),
+      },
+    };
+
+    await buildCcdCaseForPossessionClaimResponse(req, possessionClaimResponse);
+  },
   extendGetContent: req => {
     //TODO: get claimantName from CCD case - currently hardcoded
     const claimantName = req.session?.ccdCase?.data?.claimantName || 'Treetops Housing';
