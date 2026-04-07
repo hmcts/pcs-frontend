3e5652f3c294d72bd863ed7c600c6cad70f160bd
SantoshiSDhmcts <santoshi.donavalli@hmcts.net>
2026-04-07 09:45:21 +0100

    h1 consistency

--- src/test/ui/functional/paymentInterstitial.pft.ts
+++ src/test/ui/functional/paymentInterstitial.pft.ts
@@ -9,3 +9,4 @@ export async function paymentInterstitialNavigationTests(): Promise<void> {
   await performValidation('pageNavigation', paymentInterstitial.backLink, counterClaim.mainHeader);
   await performValidation('pageNavigation', paymentInterstitial.cancelLink, dashboard.mainHeader);
 }
+// 123
