770d1c30e06dd9a4677c673165fcc814d7ddd387
adusumillipraveen <praveen.adusumilli@hmcts.net>
2026-04-08 12:25:16 +0100

    fix

--- src/main/steps/respond-to-claim/start-now/index.ts
+++ src/main/steps/respond-to-claim/start-now/index.ts
@@ -3,10 +3,11 @@ import type { Request, Response } from 'express';
 import type { StepDefinition } from '../../../interfaces/stepFormData.interface';
 import { createGetController, createStepNavigation } from '../../../modules/steps';
 import { getDashboardUrl } from '../../../routes/dashboard';
+import { getFlowConfigForJourney } from '../../../steps';
 import { RESPOND_TO_CLAIM_ROUTE, flowConfig } from '../flow.config';
 
 const stepName = 'start-now';
-const stepNavigation = createStepNavigation(flowConfig);
+const stepNavigation = createStepNavigation(req => getFlowConfigForJourney('respondToClaim', req) || flowConfig);
 
 export const step: StepDefinition = {
   url: `${RESPOND_TO_CLAIM_ROUTE}/start-now`,
