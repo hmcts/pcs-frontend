cf62461909784cd774fb4ad67749152ca284e5d3
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-04-09 22:50:01 +0100

    HDPI-5442-Add Basic E2E User Journey to Smoke Test Stage in pcs-frontend Pipeline

---
+++ src/test/ui/e2eTest/respondToAClaimSmokeTest.spec.ts
@@ -0,0 +1,27 @@
+import { test } from '@playwright/test';
+import config from 'config';
+
+import {
+  freeLegalAdvice,
+  startNow
+} from '../data/page-data';
+import { initializeExecutor, performAction } from '../utils/controller';
+import { createCaseApiData, submitCaseApiData } from '../data/api-data';
+
+const home_url = config.get('e2e.testUrl') as string;
+
+test.describe('Respond to a claim - smoke test @smoke', async () => {
+  test('Respond to a claim @smoke', async ({ page }) => {
+    initializeExecutor(page);
+    await performAction('createCaseAPI', { data: createCaseApiData.createCasePayload });
+    await performAction('submitCaseAPI', { data: submitCaseApiData.submitCasePayload });
+    await performAction('fetchPINsAPI');
+    await performAction('createUser', 'citizen', ['citizen']);
+    await performAction('validateAccessCodeAPI');
+    await performAction('navigateToUrl', home_url);
+    await performAction('login');
+    await performAction('navigateToUrl', home_url + `/case/${process.env.CASE_NUMBER}/respond-to-claim/start-now`);
+    await performAction('clickButton', startNow.startNowButton);
+    await performAction('selectLegalAdvice', freeLegalAdvice.yesRadioOption);
+  });
+});
