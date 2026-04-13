7c976dddcbdd1d2a782e668a9594e701cffaaa85
padmakumari <padma.kumari@hmcts.net>
2026-04-13 11:01:34 +0100

    updated axe_exclusions files

--- src/test/ui/config/axe-exclusions.config.ts
+++ src/test/ui/config/axe-exclusions.config.ts
@@ -16,5 +16,5 @@ export const axe_exclusions = [
   '#disputeOtherParts', //page: disputePartOfClaim, element: 'Yes', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
   '#shareCircumstances', //page: yourCircumstances, element: 'Yes', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
   '#exceptionalHardship', //page: exceptionalHardship, element: 'Yes', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
-  '#alreadyAppliedForHelp'//page : alreadyAppliedForHelp,element: 'Yes', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
+  '#alreadyAppliedForHelp', //page : alreadyAppliedForHelp,element: 'Yes', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
 ];
