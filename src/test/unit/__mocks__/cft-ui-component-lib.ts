524486af36d29c90d5b01c145341a79ded60064e
ayub-macalim <ayub.macalim@hmcts.net>
2026-04-15 14:32:48 +0100

    removed import stub

--- src/test/unit/__mocks__/cft-ui-component-lib.ts
+++
@@ -1,2 +0,0 @@
-export const buildHeaderModel = jest.fn();
-export const buildFooterModel = jest.fn();
--- src/types/cft-ui-component-lib.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@hmcts-cft/cft-ui-component-lib' {
-  interface HeaderModel {
-    assetsPath?: string;
-    [key: string]: unknown;
-  }
-
-  export function buildHeaderModel(options: { xuiBaseUrl: string; user: { roles: string[] } }): HeaderModel;
-  export function buildFooterModel(): unknown;
-}
