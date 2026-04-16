871815ab258dd05baaf27d6dbb4959b6f60a7120
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2026-04-16 09:44:56 +0100

    HDPI-3758-QA

--- src/test/ui/functional/languageUsed.pft.ts
+++ src/test/ui/functional/languageUsed.pft.ts
@@ -1,18 +1,14 @@
-import { equalityAndDiversityEnd, feedback, languageUsed } from '../data/page-data';
+import { equalityAndDiversityEnd, languageUsed } from '../data/page-data';
 import { performAction, performValidation } from '../utils/controller';
 
 export async function languageUsedErrorValidation(): Promise<void> {
   await performAction('clickButton', languageUsed.saveAndContinueButton);
   await performValidation('errorMessage', {
     header: languageUsed.thereIsAProblemErrorMessageHeader,
-    message: languageUsed.errorMessage,
+    message: languageUsed.errorHiddenMessage,
   });
 }
 
 export async function languageUsedNavigationTests(): Promise<void> {
-  await performValidation('pageNavigation', languageUsed.feedbackLink, {
-    element: feedback.tellUsWhatYouThinkParagraph,
-    pageSlug: languageUsed.pageSlug,
-  });
   await performValidation('pageNavigation', languageUsed.backLink, equalityAndDiversityEnd.mainHeader);
 }
