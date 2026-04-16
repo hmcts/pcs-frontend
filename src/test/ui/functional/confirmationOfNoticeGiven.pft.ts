3b88fa05efff2a484b9345c30504be1d7b1fc168
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-04-16 15:17:13 +0100

    commented confirmation of notice given pft due to bug

--- src/test/ui/functional/confirmationOfNoticeGiven.pft.ts
+++ src/test/ui/functional/confirmationOfNoticeGiven.pft.ts
@@ -1,22 +1,23 @@
-import { submitCaseApiData } from '../data/api-data';
+// import { submitCaseApiData } from '../data/api-data';
 import { confirmationOfNoticeGiven, dashboard, feedback, tenancyDateUnknown } from '../data/page-data';
 import { performAction, performValidation } from '../utils/controller';
 
-let claimantName = '';
-
-if (process.env.CLAIMANT_NAME_OVERRIDDEN === 'YES') {
-  claimantName = submitCaseApiData.submitCasePayloadNoDefendants.overriddenClaimantName;
-} else {
-  claimantName = submitCaseApiData.submitCasePayloadNoDefendants.claimantName;
-}
-
-export async function confirmationOfNoticeGivenErrorValidation(): Promise<void> {
-  await performAction('clickButton', confirmationOfNoticeGiven.saveAndContinueButton);
-  await performValidation('errorMessage', {
-    header: confirmationOfNoticeGiven.thereIsAProblemErrorMessageHeader,
-    message: confirmationOfNoticeGiven.selectIfNoticeOfIntentionGivenErrorMessage(claimantName),
-  });
-}
+//Bug - HDPI-6087
+// let claimantName = '';
+//
+// if (process.env.CLAIMANT_NAME_OVERRIDDEN === 'YES') {
+//   claimantName = submitCaseApiData.submitCasePayloadNoDefendants.overriddenClaimantName;
+// } else {
+//   claimantName = submitCaseApiData.submitCasePayloadNoDefendants.claimantName;
+// }
+//
+// export async function confirmationOfNoticeGivenErrorValidation(): Promise<void> {
+//   await performAction('clickButton', confirmationOfNoticeGiven.saveAndContinueButton);
+//   await performValidation('errorMessage', {
+//     header: confirmationOfNoticeGiven.thereIsAProblemErrorMessageHeader,
+//     message: confirmationOfNoticeGiven.selectIfNoticeOfIntentionGivenErrorMessage(claimantName),
+//   });
+// }
 
 export async function confirmationOfNoticeGivenNavigationTests(): Promise<void> {
   await performValidation('pageNavigation', confirmationOfNoticeGiven.feedbackLink, {
