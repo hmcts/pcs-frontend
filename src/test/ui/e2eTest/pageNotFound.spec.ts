73ba09f5b401cd27f93e38bb1f4a924d8b9ee404
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-04-20 11:09:34 +0100

    No failures- all stages should pass

--- src/test/ui/e2eTest/pageNotFound.spec.ts
+++ src/test/ui/e2eTest/pageNotFound.spec.ts
@@ -28,7 +28,6 @@ test.describe('Error page to indicate Page Not Found error @nightly', () => {
   test.skip('Invalid caseId validation', async () => {
     await performAction('navigateToUrl', home_url + '/case/1234567891234567/respond-to-claim/start-now');
     await performValidation('mainHeader', 'Sorry, we’re having technical problems');
-    await performValidation('text', { text: 'Please try again in a few minutes.', elementType: 'paragraph' });
   });
 
   // This test was written as part of the story HDPI-3883. A new story will update the error message screen with a “Contact Us” link.
