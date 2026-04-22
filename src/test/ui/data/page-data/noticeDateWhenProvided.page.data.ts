79fa5e5ffff446fd8d1dfedb5e21408ea7355b7e
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-04-22 12:27:31 +0100

    notice date known

--- src/test/ui/data/page-data/noticeDateWhenProvided.page.data.ts
+++ src/test/ui/data/page-data/noticeDateWhenProvided.page.data.ts
@@ -4,8 +4,8 @@ export const noticeDateWhenProvided = {
   mainHeader: `Notice date`,
   respondToAPropertyPossessionParagraph: `Respond to a property possession claim`,
   backLink: `Back`,
-  whenMakingClaimHintText: (claimantName: string) => `When making the claim,  had to say the date they gave you notice (the date of service). If you’re not sure of the exact date, you can find it on the notice.`,
-  noticeDetailsGivenLabel: `Notice details given by Possession Claims Solicitor Org:`,
+  whenMakingClaimHintText: (claimantName: string) => `When making the claim, ${claimantName} had to say the date they gave you notice (the date of service). If you’re not sure of the exact date, you can find it on the notice.`,
+  noticeDetailsGivenLabel: (claimantName: string) => `Notice details given by ${claimantName}:`,
   noticeGivenDateLabel: `They served you with a notice seeking possession on ${convertDateFormat(submitCaseApiData.submitCasePayload.notice_NoticePostedDate)}`,
   getWhenDidYouReceiveNoticeQuestion: (claimantName: string) =>
     `When did you receive notice from ${claimantName} (optional)?`,
