e12040e8937509b3ddd4511d2a22601f02b92066
sstewart <scott.stewart@solirius.com>
2026-04-22 20:09:39 +0100

    HDPI-4272: Clean up CYA page and add statement of truth

--- src/main/services/ccdCase.interface.ts
+++ src/main/services/ccdCase.interface.ts
@@ -222,10 +222,10 @@ export interface StartCallbackData {
 }
 
 export enum GenAppType {
-  SUSPEND,
-  ADJOURN,
-  SET_ASIDE,
-  SOMETHING_ELSE,
+  SUSPEND = 'SUSPEND',
+  ADJOURN = 'ADJOURN',
+  SET_ASIDE = 'SET_ASIDE',
+  SOMETHING_ELSE = 'SOMETHING_ELSE',
 }
 
 export interface CitizenGenAppRequest {
@@ -239,4 +239,6 @@ export interface CitizenGenAppRequest {
   withoutNoticeReason?: string;
   languageUsed?: LanguageUsed;
   whatOrderWanted?: string;
+  sotAccepted?: YesNoValue;
+  sotFullName?: string;
 }
--- src/main/steps/make-an-application/check-your-answers/index.ts
+++ src/main/steps/make-an-application/check-your-answers/index.ts
@@ -1,86 +1,115 @@
-import type { Request, Response } from 'express';
+import type { Request } from 'express';
 import type { TFunction } from 'i18next';
 
-import { createGetController, createStepNavigation, getTranslationFunction } from '../../../modules/steps';
+import { createFormStep, getFormData, getTranslationFunction } from '../../../modules/steps';
 import { ccdCaseService } from '../../../services/ccdCaseService';
 import { toYesNoEnum } from '../../utils';
-import { MAKE_AN_APPLICATION_ROUTE, flowConfig } from '../flow.config';
+import { flowConfig } from '../flow.config';
 
 import { buildSummaryListRows } from './summaryListRowFactory';
 import VisibleFormDataView from './visibleFormDataView';
 
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
-import { CitizenGenAppRequest } from '@services/ccdCase.interface';
+import { CitizenGenAppRequest, GenAppType } from '@services/ccdCase.interface';
 
 const STEP_NAME = 'check-your-answers';
-const stepNavigation = createStepNavigation(flowConfig);
 
-export const step: StepDefinition = {
-  url: `${MAKE_AN_APPLICATION_ROUTE}/check-your-answers`,
-  name: STEP_NAME,
-  view: 'make-an-application/check-your-answers/checkYourAnswers.njk',
+export const step: StepDefinition = createFormStep({
+  stepName: STEP_NAME,
+  journeyFolder: 'makeAnApplication',
   stepDir: __dirname,
-  getController: () => {
-    return createGetController(
-      'make-an-application/check-your-answers/checkYourAnswers.njk',
-      STEP_NAME,
-      stepNavigation,
-      (req: Request) => {
-        const t: TFunction = getTranslationFunction(req, STEP_NAME, ['common']);
-
-        return {
-          summaryData: {
-            rows: buildSummaryListRows(req, t),
-          },
-        };
-      },
-      'makeAnApplication'
-    );
-  },
-  postController: {
-    post: async (req: Request, res: Response) => {
-      const formData = req.session.formData;
-
-      const ccdCase = res.locals.validatedCase;
-      if (!ccdCase) {
-        throw Error('No existing case details in session');
-      }
-
-      if (!formData) {
-        throw Error('No existing formData in session');
-      }
-
-      const visibleFormData = new VisibleFormDataView(req);
-
-      const citizenGenAppRequest: CitizenGenAppRequest = {
-        applicationType: visibleFormData.getApplicationTypeField()?.fieldValue,
-        within14Days: toYesNoEnum(visibleFormData.getHearingInNext14DaysField()?.fieldValue),
-        needHwf: toYesNoEnum(visibleFormData.getHelpWithFeesNeededField()?.fieldValue),
-        appliedForHwf: toYesNoEnum(visibleFormData.getAlreadyAppliedForHwfField()?.fieldValue),
-        hwfReference: visibleFormData.getHwfReferenceField()?.fieldValue,
-        otherPartiesAgreed: toYesNoEnum(visibleFormData.getOtherPartiesAgreedField()?.fieldValue),
-        withoutNotice: toYesNoEnum(visibleFormData.getAnyReasonsNotToShareField()?.fieldValue),
-        withoutNoticeReason: visibleFormData.getReasonForNotSharingField()?.fieldValue,
-        languageUsed: visibleFormData.getWhichLanguageField()?.fieldValue,
-        whatOrderWanted: visibleFormData.getWhatOrderWantedField()?.fieldValue,
-      };
-
-      await ccdCaseService.submitGeneralApplication(req.session?.user?.accessToken, {
-        id: ccdCase.id,
-        data: {
-          citizenGenAppRequest,
+  flowConfig,
+  customTemplate: `${__dirname}/checkYourAnswers.njk`,
+  fields: [
+    {
+      name: 'statementOfTruthAccepted',
+      type: 'checkbox',
+      required: true,
+      errorMessage: 'errors.agreeToTheStatementOfTruth',
+      translationKey: { label: 'statementOfTruth.checkbox.fieldLabel' },
+      legendClasses: 'govuk-visually-hidden',
+      options: [
+        {
+          value: 'yes',
+          translationKey: 'statementOfTruth.checkbox.value',
         },
-      });
-
-      delete req.session.formData;
-
-      const redirectPath = await stepNavigation.getNextStepUrl(req, STEP_NAME, req.body);
-
-      if (!redirectPath) {
-        return res.status(500).send('Unable to determine next step');
-      }
-
-      res.redirect(303, redirectPath);
+      ],
     },
+    {
+      name: 'fullName',
+      type: 'text',
+      required: true,
+      maxLength: 100,
+      labelClasses: 'govuk-label--s',
+      errorMessage: 'errors.youMustSignYourName',
+      translationKey: {
+        label: 'statementOfTruth.yourFullName',
+      },
+    },
+  ],
+  translationKeys: {
+    pageTitle: 'pageTitle',
+    caption: 'caption',
+    heading: 'heading',
+    statementOfTruthSubheading: 'statementOfTruth.subheading',
+    theInformationFormsYourApplication: 'statementOfTruth.theInformationFormsYourApplication',
+    whenYouAreSatisfied: 'statementOfTruth.whenYouAreSatisfied',
   },
-};
+  extendGetContent: async (req: Request) => {
+    const t: TFunction = getTranslationFunction(req, STEP_NAME, ['common']);
+
+    const visibleFormData = new VisibleFormDataView(req);
+
+    const feeApplies =
+      visibleFormData.getApplicationTypeField()?.fieldValue !== GenAppType.ADJOURN ||
+      visibleFormData.getHearingInNext14DaysField()?.fieldValue === 'yes';
+
+    return {
+      summaryData: {
+        rows: buildSummaryListRows(req, t),
+      },
+      buttonLabel: feeApplies ? t('buttons.continueToPayment') : t('buttons.submit'),
+    };
+  },
+  beforeRedirect: async (req: Request) => {
+    const formData = req.session.formData;
+
+    const ccdCase = req.res?.locals.validatedCase;
+    if (!ccdCase) {
+      throw Error('No existing case details in session');
+    }
+
+    if (!formData) {
+      throw Error('No existing formData in session');
+    }
+
+    const cyaFormData = getFormData(req, STEP_NAME);
+    const statementOfTruthAccepted = (cyaFormData.statementOfTruthAccepted as string[])[0] as 'yes' | 'no';
+
+    const visibleFormData = new VisibleFormDataView(req);
+
+    const citizenGenAppRequest: CitizenGenAppRequest = {
+      applicationType: visibleFormData.getApplicationTypeField()?.fieldValue,
+      within14Days: toYesNoEnum(visibleFormData.getHearingInNext14DaysField()?.fieldValue),
+      needHwf: toYesNoEnum(visibleFormData.getHelpWithFeesNeededField()?.fieldValue),
+      appliedForHwf: toYesNoEnum(visibleFormData.getAlreadyAppliedForHwfField()?.fieldValue),
+      hwfReference: visibleFormData.getHwfReferenceField()?.fieldValue,
+      otherPartiesAgreed: toYesNoEnum(visibleFormData.getOtherPartiesAgreedField()?.fieldValue),
+      withoutNotice: toYesNoEnum(visibleFormData.getAnyReasonsNotToShareField()?.fieldValue),
+      withoutNoticeReason: visibleFormData.getReasonForNotSharingField()?.fieldValue,
+      languageUsed: visibleFormData.getWhichLanguageField()?.fieldValue,
+      whatOrderWanted: visibleFormData.getWhatOrderWantedField()?.fieldValue,
+      sotAccepted: toYesNoEnum(statementOfTruthAccepted),
+      sotFullName: cyaFormData.fullName as string,
+    };
+
+    await ccdCaseService.submitGeneralApplication(req.session?.user?.accessToken, {
+      id: ccdCase.id,
+      data: {
+        citizenGenAppRequest,
+      },
+    });
+
+    delete req.session.formData;
+  },
+});
