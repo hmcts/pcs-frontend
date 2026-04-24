bb6fc2a594673ebd5042ef45ec2621a65c0ad413
lovelin <lovelin.marcus@hmcts.net>
2026-04-24 10:44:32 +0100

    updated to create a case instead of submit a case

--- src/test/ui/data/page-data/dashboard.page.data.ts
+++ src/test/ui/data/page-data/dashboard.page.data.ts
@@ -1,24 +1,27 @@
 import { createCaseApiData } from '../api-data';
-import { submitCaseApiDataWales } from '../api-data/submitCaseWales.api.data';
+import { createCaseApiWalesData } from '../api-data/createCaseWales.api.data';
 
 export const dashboard = {
   get mainHeader() {
     return process.env.WALES_POSTCODE === 'YES'
-      ? `${submitCaseApiDataWales.submitCasePayload.propertyAddress.AddressLine1}, ${submitCaseApiDataWales.submitCasePayload.propertyAddress.PostTown}, ${submitCaseApiDataWales.submitCasePayload.propertyAddress.PostCode}`
+      ? `${createCaseApiWalesData.createCasePayload.propertyAddress.AddressLine1}, ${createCaseApiWalesData.createCasePayload.propertyAddress.PostTown}, ${createCaseApiWalesData.createCasePayload.propertyAddress.PostCode}`
       : `${createCaseApiData.createCasePayload.propertyAddress.AddressLine1}, ${createCaseApiData.createCasePayload.propertyAddress.PostTown}, ${createCaseApiData.createCasePayload.propertyAddress.PostCode}`;
   },
   caseNumberParagraph: (): string => `Case number: ${process.env.CASE_FID}`,
+  iWantToHeader: 'I want to...',
+  askTheCourtToMakeAnOrderLink: 'Ask the court to make an order (make a general application)',
+  uploadAdditionalDocumentsLink: 'Upload additional documents',
   helpAndSupportHeader: `Help and support`,
   helpWithFeesLink: `Help with fees`,
   getHelpPayingCourtFeesHeader: `Get help paying court and tribunal fees`,
+  findOutAboutMediationLink: `Find out about mediation`,
+  aGuideToCivilMediationHeader: `A guide to civil mediation`,
   whatToExpectAtHearingLink: `What to expect at a hearing`,
   whatToExpectComingCourtHeader: `What to expect coming to a court or tribunal`,
   representMyselfAtHearingLink: `Represent myself at a hearing`,
   representYourselfHeader: `Represent yourself in court`,
   findLegalAdviceLink: `Find legal advice`,
   findLegalAdviceHeader: `Find legal advice and information`,
-  getDebtRespiteLink: `Get debt respite (Breathing Space)`,
-  breathingSpaceHeader: `Breathing Space (Debt Respite Scheme)`,
   findInfoAboutMyCourtLink: `Find information about my court`,
   findACourtOrTribunalHeader: `Find a court or tribunal`,
 };
