74446cd6e73b3fe7fd2450bcb0dd941c74cb1724
bytesize-hq <bytesize-hq@gmail.com>
2026-04-24 13:03:14 +0100

    fix lint issues

--- src/main/routes/hearings.ts
+++ src/main/routes/hearings.ts
@@ -3,7 +3,11 @@ import { Application, Request, Response } from 'express';
 import { oidcMiddleware } from '../middleware';
 
 export default function hearingsRoutes(app: Application): void {
-  app.get('/dashboard/:caseReference/hearing/Defendant.ViewHearingDocuments', oidcMiddleware, (req: Request, res: Response) => {
-    res.render('view-hearing-documents');
-  });
+  app.get(
+    '/dashboard/:caseReference/hearing/Defendant.ViewHearingDocuments',
+    oidcMiddleware,
+    (req: Request, res: Response) => {
+      res.render('view-hearing-documents');
+    }
+  );
 }
