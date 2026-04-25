73e50dfed640f30c88b491b1c374114f918526b2
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-04-25 21:58:41 +0100

    Updated with latest steps adde in master

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,6 @@ if (enable_all_page_functional_tests.toLowerCase() === 'true') {
 export const enable_pft_debug_log = false;
 export const enable_content_validation = process.env.ENABLE_CONTENT_VALIDATION || 'false';
 export const enable_visibility_validation = process.env.ENABLE_VISIBILITY_VALIDATION || 'false';
-export const enable_error_message_validation_new = process.env.ENABLE_ERROR_MESSAGES_VALIDATION_NEW || 'true';
 export const enable_error_message_validation = process.env.ENABLE_ERROR_MESSAGES_VALIDATION || 'false';
 export const enable_navigation_tests = process.env.ENABLE_NAVIGATION_TESTS || 'false';
 export const enable_axe_audit = process.env.ENABLE_AXE_AUDIT || 'true';
