5df98e3d10aa151f722b7fdd101579c991445fcd
SameenaHMCTS <sameena.javeed@hmcts.net>
2026-04-25 21:43:47 +0100

    Refactored the code

--- src/test/ui/functional/languageUsed.pft.ts
+++ src/test/ui/functional/languageUsed.pft.ts
@@ -1,14 +1,14 @@
-import { equalityAndDiversityEnd, languageUsed as languageUsedPage } from '../data/page-data';
+import { equalityAndDiversityEnd, languageUsed } from '../data/page-data';
 import { performAction, performValidation } from '../utils/controller';
 
 export async function languageUsedErrorValidation(): Promise<void> {
-  await performAction('clickButton', languageUsedPage.saveAndContinueButton);
+  await performAction('clickButton', languageUsed.saveAndContinueButton);
   await performValidation('errorMessage', {
-    header: languageUsedPage.thereIsAProblemErrorMessageHeader,
-    message: languageUsedPage.errorHiddenMessage,
+    header: languageUsed.thereIsAProblemErrorMessageHeader,
+    message: languageUsed.errorHiddenMessage,
   });
 }
 
 export async function languageUsedNavigationTests(): Promise<void> {
-  await performValidation('pageNavigation', languageUsedPage.backLink, equalityAndDiversityEnd.mainHeader);
+  await performValidation('pageNavigation', languageUsed.backLink, equalityAndDiversityEnd.mainHeader);
 }
