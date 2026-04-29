3f2d111ea44362cc0f7d7692d7c98dbf7d8b67af
padmakumari <padma.kumari@hmcts.net>
2026-04-29 10:10:50 +0100

    updated the Header content for whatOrderDoYouWantTheCourtToMakeAndWhy page.

--- src/test/ui/data/page-data/genApps-page-data/whatOrderDoYouWantTheCourtToMakeAndWhy.page.data.ts
+++ src/test/ui/data/page-data/genApps-page-data/whatOrderDoYouWantTheCourtToMakeAndWhy.page.data.ts
@@ -1,5 +1,5 @@
 export const whatOrderDoYouWantTheCourtToMakeAndWhy = {
-  mainHeader: `What order do you want the court to make and why?`,
+  mainHeader: `What order do you want the court to make, and why?`,
   continueButton: `Continue`,
   cancelLink: `Cancel`,
 };
