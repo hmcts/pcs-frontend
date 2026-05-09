a4559db9f0f4f1e33cc3589894c17b50c9b9a21e
Simon Middleton <simon.middleton@solirius.com>
2026-05-09 15:27:20 +0100

    lint fix

--- src/main/steps/utils/counterClaimUploadWanted.ts
+++ src/main/steps/utils/counterClaimUploadWanted.ts
@@ -2,7 +2,7 @@ import type { Request } from 'express';
 
 export const counterClaimUploadWanted = (req: Request): boolean => {
   return (
-    req.res?.locals?.validatedCase?.data?.possessionClaimResponse?.defendantResponses
-      ?.counterClaimWantToUploadFiles === 'YES'
+    req.res?.locals?.validatedCase?.data?.possessionClaimResponse?.defendantResponses?.counterClaimWantToUploadFiles ===
+    'YES'
   );
 };
