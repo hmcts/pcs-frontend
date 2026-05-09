0fe68a740577d04b59419c31398ca8c9fa1138b4
Simon Middleton <simon.middleton@solirius.com>
2026-05-09 15:52:46 +0100

    index njks and locales for upload files page

--- src/main/steps/respond-to-claim/counter-claim-upload-files/index.ts
+++ src/main/steps/respond-to-claim/counter-claim-upload-files/index.ts
@@ -1,13 +1,53 @@
+import { RESPOND_TO_CLAIM_DRAFT_EVENT } from '../draftEvent';
 import { flowConfig } from '../flow.config';
 
+import { createCcdDraftStorage, toDisplayDocuments } from '@modules/documents/storage';
 import { createFormStep } from '@modules/steps';
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
+import { ACCEPT_ATTRIBUTE_EXTENSIONS, UPLOAD_MAX_FILE_SIZE_MB } from '@utils/documentUploadValidation';
+
+const storage = createCcdDraftStorage({
+  event: RESPOND_TO_CLAIM_DRAFT_EVENT,
+  getDocs: data =>
+    data.possessionClaimResponse?.defendantResponses?.counterClaimDocuments ?? [],
+  setDocs: docs => ({
+    possessionClaimResponse: {
+      defendantResponses: { counterClaimDocuments: docs },
+    },
+  }),
+});
 
 export const step: StepDefinition = createFormStep({
   stepName: 'counter-claim-upload-files',
   journeyFolder: 'respondToClaim',
+  documentStorage: storage,
   stepDir: __dirname,
   flowConfig,
-  fields: [],
   customTemplate: `${__dirname}/counterClaimUploadFiles.njk`,
+  fields: [
+    {
+      name: 'documents',
+      type: 'file',
+      required: false,
+      accept: ACCEPT_ATTRIBUTE_EXTENSIONS,
+      maxFileSize: UPLOAD_MAX_FILE_SIZE_MB,
+      labelClasses: 'govuk-label--s',
+      translationKey: { label: 'uploadLabel' },
+    },
+  ],
+  translationKeys: {
+    pageTitle: 'pageTitle',
+    caption: 'caption',
+    heading: 'heading',
+    guidanceText: 'guidanceText',
+    guidanceText2: 'guidanceText2',
+    beforeUploadHeading: 'beforeUploadHeading',
+    beforeUploadText: 'beforeUploadText',
+    fileTypesText: 'fileTypesText',
+    uploadLabel: 'uploadLabel',
+    filesAddedHeading: 'filesAddedHeading',
+    uploadButton: 'uploadButton',
+    deleteButton: 'deleteButton',
+  },
+  getInitialFormData: async req => ({ documents: toDisplayDocuments(await storage.read(req)) }),
 });
