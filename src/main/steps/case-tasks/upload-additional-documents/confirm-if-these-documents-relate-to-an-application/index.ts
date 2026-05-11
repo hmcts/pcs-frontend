b83beee58a34c8157899e7c5d4940090e2a0072d
Marianne-Azzopardi <marianne.azzopardi@solirius.com>
2026-05-11 12:56:50 +0100

    add initial confirm existing application upload page

--- src/main/steps/case-tasks/upload-additional-documents/confirm-if-these-documents-relate-to-an-application/index.ts
+++ src/main/steps/case-tasks/upload-additional-documents/confirm-if-these-documents-relate-to-an-application/index.ts
@@ -12,6 +12,7 @@ const journeyName = 'uploadAdditionalDocuments';
 const stepName = 'confirm-if-these-documents-relate-to-an-application';
 const templatePath =
   'case-tasks/upload-additional-documents/confirm-if-these-documents-relate-to-an-application/confirmIfTheseDocumentsRelateToAnApplication.njk';
+
 const stepNavigation = createStepNavigation(req => getFlowConfigForJourney(journeyName, req) || flowConfig);
 
 export const step: StepDefinition = {
@@ -21,6 +22,25 @@ export const step: StepDefinition = {
   stepDir: __dirname,
   getController: () =>
     createGetController(templatePath, stepName, stepNavigation, (req: Request) => ({
-      dashboardUrl: getDashboardUrl(req.res?.locals.validatedCase?.id),
+      backUrl: getDashboardUrl(req.res?.locals.validatedCase?.id) ?? '/dashboard',
+        dashboardUrl: getDashboardUrl(req.res?.locals.validatedCase?.id),
+        applications: [
+          {
+            value: 'gen-app-1',
+            text: "Yes, the documents I'm uploading relate to the application to adjourn the hearing - submitted on Monday 28 April 2026",
+          },
+          {
+            value: 'gen-app-2',
+            text: "Yes, the documents I'm uploading relate to the application to set aside an order - submitted on Wednesday 16 April 2026",
+          },
+          {
+            value: 'gen-app-3',
+            text: "Yes, the documents I'm uploading relate to the application submitted on Tuesday 8 April 2026",
+          },
+          {
+            value: 'claim-or-counterclaim',
+            text: "No, the documents I'm uploading relate to the claim or counterclaim",
+          },
+        ],
     })),
 };
