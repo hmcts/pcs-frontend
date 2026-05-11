bb896c8c2001e6bf791a114c5d8db91968e05991
sstewart <202802468+scottstewart-sl@users.noreply.github.com>
2026-05-11 18:07:25 +0100

    Fix Redis lint issue following dependency update

--- src/main/modules/session/index.ts
+++ src/main/modules/session/index.ts
@@ -1,5 +1,5 @@
 import config from 'config';
-import RedisStore from 'connect-redis';
+import { RedisStore } from 'connect-redis';
 import type { Express } from 'express';
 import session from 'express-session';
 import { Redis } from 'ioredis';
@@ -28,8 +28,7 @@ export class Session {
 
     app.locals.redisClient = redis;
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const redisStore = new (RedisStore as any)({
+    const redisStore = new RedisStore({
       client: redis,
       prefix: config.get('session.prefix') + ':',
       ttl: config.get('session.redis.ttlInSeconds'),
