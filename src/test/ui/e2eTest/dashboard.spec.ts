6a05df96c3f00d6cdd354eb3700699744891f9c3
lovelin <lovelin.marcus@hmcts.net>
2026-05-13 13:30:22 +0100

    Updated test title

--- src/test/ui/e2eTest/dashboard.spec.ts
+++ src/test/ui/e2eTest/dashboard.spec.ts
@@ -28,7 +28,7 @@ test.beforeEach(async ({ page }, testInfo) => {
 });
 
 test.describe('Dashboard - e2e Journey @nightly', async () => {
-  test('Validate address, case number and links on the dashboard @smoke @crossbrowser', async () => {
+  test('Validate address, case number and links on the dashboard @smoke @regression @crossbrowser', async () => {
     await performValidation('mainHeader', dashboard.mainHeader);
     await performValidation('text', { elementType: 'paragraph', text: dashboard.caseNumberParagraph() });
     await performActions(
