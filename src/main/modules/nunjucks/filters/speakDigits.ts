62bf407d82accbf79b0cded0305426b3f1774b3a
adusumillipraveen <praveen.adusumilli@hmcts.net>
2026-05-15 10:02:24 +0100

    revert

--- src/main/modules/nunjucks/filters/speakDigits.ts
+++
@@ -1,8 +0,0 @@
-export const speakDigits = (value: string | number | null | undefined): string => {
-  if (value === null || value === undefined) {
-    return '';
-  }
-
-  const digits = String(value).replace(/\D/g, '');
-  return digits.split('').join(', ');
-};
