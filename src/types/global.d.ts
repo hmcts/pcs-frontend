f04d170b32a36e2ffe593e3b4f6b317055bc7ccc
Paddy Basi <pardeep.basi@hmcts.net>
2026-05-18 15:07:56 +0100

    HDPI-4270: cui genapps document upload (#1259)
    Co-authored-by: srinijg <srinivasan.joghee@hmcts.net>
    Co-authored-by: PadmaDeenadayalan <Padma.Deenadayalan@hmcts.net>
    Co-authored-by: padmakumari <padma.kumari@hmcts.net>
    Co-authored-by: arun <arun.kumar@hmcts.net>
    Co-authored-by: scottstewart-sl <scott.stewart@solirius.com>
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
    Co-authored-by: arun-hmcts <73122505+arun-hmcts@users.noreply.github.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: marianadpereira <mariana.pereira@HMCTS.NET>
    Co-authored-by: madhavi1203 <Madhavi.Atmakuri1@HMCTS.NET>

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -20,6 +20,7 @@ interface CustomSessionData extends SessionData {
   user?: UserInfoResponseWithToken;
   returnTo?: string;
   formData?: Record<string, any>;
+  uploadedDocs?: Record<string, Record<string, unknown[]>>;
   ccdCase?: CcdCase;
   genApp?: {
     applicationId?: string;
