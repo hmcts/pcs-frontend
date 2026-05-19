8c4c0a1dd6ddaceb2c889524d345a3e684523a42
Simon Middleton <simon.middleton@solirius.com>
2026-05-19 13:18:32 +0100

    linting issue

--- src/test/unit/steps/respond-to-claim/counter-claim-hwf.test.ts
+++ src/test/unit/steps/respond-to-claim/counter-claim-hwf.test.ts
@@ -238,10 +238,8 @@ describe('respond-to-claim counter-claim HWF show conditions', () => {
   describe('counter-claim-you-need-to-apply showCondition', () => {
     const showCondition = flowConfig.steps['counter-claim-you-need-to-apply-for-help-with-your-fees']?.showCondition;
 
-     
-    const makeNeedToApplyReq = (
-      counterClaim: { needHelpWithFees?: string; appliedForHwf?: string } | undefined
-    ): any => ({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const makeNeedToApplyReq = (counterClaim: { needHelpWithFees?: string; appliedForHwf?: string } | undefined): any => ({
       res: {
         locals: {
           validatedCase: {
