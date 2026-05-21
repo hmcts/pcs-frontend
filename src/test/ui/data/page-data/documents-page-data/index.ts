4c4f361e4413f7bc29c5e5728648a3c2c6e9dbe9
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2026-05-21 09:15:58 +0100

    view documents screen (#1276)

--- src/test/ui/data/page-data/documents-page-data/index.ts
+++ src/test/ui/data/page-data/documents-page-data/index.ts
@@ -1,3 +1,4 @@
 export * from './startEvidenceUpload.page.data';
 export * from './confirmIfTheseDocumentsRelateToAnApplication.page.data';
 export * from './uploadYourDocuments.page.data';
+export * from './viewDocuments.page.data';
