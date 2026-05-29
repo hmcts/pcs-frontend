e00b6495b9b28e664051aea774216fa8357aac1f
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-05-29 14:39:39 +0100

    updated data file

--- src/test/ui/data/page-data/taskList.page.data.ts
+++ src/test/ui/data/page-data/taskList.page.data.ts
@@ -4,6 +4,18 @@ export const taskList = {
   get mainHeader(): string {
     return `${address}`;
   },
+  caseNumberParagraph: (): string => `Case number: ${process.env.CASE_FID}`,
+  iWantToHeader: 'I want to...',
+  makeAGeneralApplicationLink: `Make a general application`,
+  informTheCourtLink: `Inform the court of a breathing space debt respite`,
+  getFreeHelpLink: `Get free help from a legal adviser`,
+  helpAndSupportHeader: `Help and support`,
+  helpWithFeesLink: `Help with fees`,
+  findOutAboutMediationLink: `Find out about mediation`,
+  whatToExpectLink: `What to expect at a hearing`,
+  representMyselfLink: `Represent myself at a hearing`,
+  findLegalAdviceLink: `Find legal advice`,
+  findInformationLink: `Find information about my court`,
   backLink: `Back`,
   checkBeforeYouStartHeading: `1. Check before you start`,
   readInformationAboutLink: `Read information about responding and free legal advice`,
