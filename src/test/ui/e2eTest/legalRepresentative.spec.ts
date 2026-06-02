41589932561e88e8f4a2283cc117e862148b7955
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2026-06-02 16:38:26 +0100

    payload issue

--- src/test/ui/e2eTest/legalRepresentative.spec.ts
+++ src/test/ui/e2eTest/legalRepresentative.spec.ts
@@ -9,14 +9,18 @@ const home_url = process.env.TEST_URL;
 
 test.beforeEach(async ({ page }, testInfo) => {
   initializeExecutor(page);
+  process.env.NOTICE_SERVED = 'YES';
+  process.env.CLAIMANT_NAME = submitCaseApiData.submitCasePayloadNoDefendants.overriddenClaimantName;
+  process.env.CLAIMANT_NAME_OVERRIDDEN = 'YES';
+  process.env.CORRESPONDENCE_ADDRESS = 'UNKNOWN';
   logTestEnvAfterBeforeEach(testInfo.title, RESPOND_TO_CLAIM_WALES_BEFORE_EACH_ENV_KEYS);
   await performAction('createCaseAPI', { data: createCaseApiData.createCasePayload });
   await performAction('submitCaseAPI', { data: submitCaseApiData.submitCasePayloadNoDefendants });
   await performAction('getCaseAPI');
   await performAction('navigateToUrl', home_url);
   await performAction('login', user.defendantSolicitor.email);
-  await performAction('navigateToUrl', home_url + `/access-your-case`);
-  await performAction('accessYourCase', { caseNumber: process.env.CASE_NUMBER });
+  /*await performAction('navigateToUrl', home_url + `/access-your-case`);
+  await performAction('accessYourCase', { caseNumber: process.env.CASE_NUMBER });*/
   await performAction('navigateToUrl', home_url + `/case/${process.env.CASE_NUMBER}/respond-to-claim/start-now`);
   await performAction('clickButton', startNow.startNowButton);
 });
@@ -25,8 +29,8 @@ test.afterEach(async () => {
   finaliseAllValidations();
 });
 
-test.describe('Respond to a claim - e2e Journey @nightly', async () => {
-  test('Respond to a claim - Wales - Secure contract - RentArrears and NonRentArrears - SelectCounterClaim - Yes - CounterClaimFee - INeedHelp @PR @noDefendants @smoke @regression', async () => {
+test.describe('Respond to a claim LR - e2e Journey @nightly', async () => {
+  test('LR Automation @noDefendants @smoke @regression', async () => {
     //Single named party - A sum of money or comp - specific sum of money (Yes) - counterclaimFee- I need help
     await performAction('selectLegalAdvice', freeLegalAdvice.yesRadioOption);
     await performAction('inputDefendantDetails', {
