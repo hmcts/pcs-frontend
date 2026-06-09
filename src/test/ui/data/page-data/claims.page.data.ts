40fe447c426c0bb46c1a48700e7b3d8c39675875
Gautham059 <gautham.bhikkumalla@hmcts.net>
2026-06-09 13:17:04 +0100

    typo correction

--- src/test/ui/data/page-data/claims.page.data.ts
+++ src/test/ui/data/page-data/claims.page.data.ts
@@ -1,5 +1,5 @@
 export const claims = {
-  mainHeader: `Your Possessiohn Claims account`,
+  mainHeader: `Your Possession Claims account`,
   ifAClaimYouWantToViewParagraph: `If a claim you want to view is not listed below, you must first `,
   provideSomeDetailsLink: `provide some details to access it (opens in new tab)`,
   claimsMadeAgainstYouSubHeader: `Claims made against you`,
