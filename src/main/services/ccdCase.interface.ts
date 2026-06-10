02bc1708dbf97e74a39dfade58a559b8fc2c26ec
Marianne-Azzopardi <marianne.azzopardi@solirius.com>
2026-06-10 11:14:32 +0100

    fix merge conflict

--- src/main/services/ccdCase.interface.ts
+++ src/main/services/ccdCase.interface.ts
@@ -234,6 +234,7 @@ export interface CcdDefendantResponses {
   otherConsiderations?: YesNoValue;
   otherConsiderationsDetails?: string;
   makeCounterClaim?: YesNoValue;
+  counterClaimWantToUploadFiles?: YesNoValue;
   statementOfTruthCompletedBy?: string;
   completedSections?: RespondToClaimSectionEnum[];
 }
