9729bfc5bd17f8e7ceac2bb0a6efe5070684fe33
padmakumari <padma.kumari@hmcts.net>
2026-06-11 12:03:15 +0100

    Updated the files

--- src/test/ui/config/axe-exclusions.config.ts
+++ src/test/ui/config/axe-exclusions.config.ts
@@ -25,5 +25,5 @@ export const axe_exclusions = [
   '#reasonsAppShouldNotBeShared', //page : reasonsAppShouldNotBeShared,element: 'Yes', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
   '#isClaimAmountKnown-2', //page: claimAmountKnown, element: 'No', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
   '#isClaimAmountKnown', //,page: claimAmountKnown, element: 'Yes', element_type: radioOption, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
-  '#address-country',//page: payForApplication, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
+  '#address-country', //page: payForApplication, reason_for_exclusion: 'aria-expanded' attribute is expected for this radio button
 ];
