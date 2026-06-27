ac1e835f6147b23930b09168971a4a9f94e9b7dd
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2026-06-27 11:46:21 +0100

    navigation fix

--- src/test/ui/functional/defendantNameCapture.pft.ts
+++ src/test/ui/functional/defendantNameCapture.pft.ts
@@ -59,6 +59,7 @@ export async function defendantNameCaptureNavigationTests(): Promise<void> {
 export async function defendantNameCaptureInputValuesPrePopulated(): Promise<void> {
   await performAction('inputText', defendantNameCapture.firstNameTextLabel, defendantNameCapture.firstNameTextInput);
   await performAction('inputText', defendantNameCapture.lastNameTextLabel, defendantNameCapture.lastNameTextInput);
+  await performValidation('pageNavigation', defendantNameCapture.saveForLaterButton, taskList.mainHeader);
   await performValidation(
     'inputTextValue',
     defendantNameCapture.firstNameTextLabel,
