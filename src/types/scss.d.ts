7bb56292658767f5a55254d9262db78f94388636
arun <arun.kumar@hmcts.net>
2026-07-06 10:19:31 +0100

    HDPI-7118: document scss.d.ts ambient declaration

--- src/types/scss.d.ts
+++ src/types/scss.d.ts
@@ -1,1 +1,3 @@
+// Side-effect scss imports are handled by webpack (style/css/sass loaders);
+// typed as an opaque module so tsc doesn't error on them (TS2882 under TS 6+).
 declare module '*.scss';
