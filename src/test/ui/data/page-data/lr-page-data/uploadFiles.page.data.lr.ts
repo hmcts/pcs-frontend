d630baf8c4edc50fd2954a1c3229659027fed492
SantoshiSDhmcts <santoshi.donavalli@hmcts.net>
2026-07-30 13:35:32 +0100

    included content validation

--- src/test/ui/data/page-data/lr-page-data/uploadFiles.page.data.lr.ts
+++ src/test/ui/data/page-data/lr-page-data/uploadFiles.page.data.lr.ts
@@ -1,9 +1,9 @@
 export const uploadAdditionalDocumentsLR = {
-  mainHeader: `Upload additional documents`,
+  mainHeader: `Upload additional documents (Optional)`,
   uploadAnyFilesParagraph: `Upload any documents that you think are relevant to the response. The claimant will be able to see any documents you upload.`,
   beforeYouUploadYourFilesParagraph: `Before you upload your documents`,
   giveYourFileANameParagraph: `Give your document a name that explains what it is.`,
-  uploadFileParagraph: `Upload a document to the system`,
+  uploadFileParagraph: `Upload a document`,
   chooseFileButton: `Choose files`,
   saveAndContinueButton: `Save and continue`,
   saveForLaterButton: `Save for later`,
