3e1a188083f179dcfa95319f4011a0282786020a
madhavi <Madhavi.Atmakuri1@HMCTS.NET>
2026-08-06 20:44:46 +0100

    date format

--- src/test/ui/utils/validations/element-validations/pdfDocument.validation.ts
+++ src/test/ui/utils/validations/element-validations/pdfDocument.validation.ts
@@ -8,19 +8,7 @@ export class PdfDocumentValidation implements IValidation {
       name: data.linkText as string,
     });
 
-    console.log('pdf link' +pdfLink);
     await expect(pdfLink).toBeVisible();
-
-    const href = await pdfLink.getAttribute('href');
-
-    expect(href).toBeTruthy();
-
-    const pdfUrl = new URL(href!, page.url());
-    const expectedPathPattern = /^\/case\/[^/]+\/view-documents\/[0-9a-fA-F-]{36}$/;
-
-    expect(
-      pdfUrl.pathname,
-      `Document "${data.linkText}" should link to an internal document route, but got "${pdfUrl.pathname}"`
-    ).toMatch(expectedPathPattern);
+    await expect(pdfLink).toHaveAttribute('href', /\/case\/[^/]+\/view-documents\/[0-9a-fA-F-]{36}/);
   }
 }
