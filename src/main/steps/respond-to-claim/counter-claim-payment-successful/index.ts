366d63db149dd14bc47424b408ba7886642681d8
danielmillard <dmillard91@hotmail.co.uk>
2026-08-10 11:38:43 +0100

    HDPI-6643: card payment success redirect

--- src/main/steps/respond-to-claim/counter-claim-payment-successful/index.ts
+++ src/main/steps/respond-to-claim/counter-claim-payment-successful/index.ts
@@ -1,8 +1,15 @@
+import config from 'config';
+
+import { isLegalRepresentativeUser } from '../../utils';
 import { createRespondToClaimFormStep } from '../formStep';
 
 import { getTranslationFunction } from '@modules/steps';
 import type { StepDefinition } from '@modules/steps/stepFormData.interface';
+import { getDashboardUrl } from '@routes/dashboard';
 import { clientContextSessionClearer } from '@utils/clientContextSessionClearer';
+import { buildManageCaseDetailsRedirect } from '@utils/manageCaseRedirect';
+
+let redirectUrl;
 
 export const step: StepDefinition = createRespondToClaimFormStep({
   stepName: 'counter-claim-payment-successful',
@@ -26,9 +33,20 @@ export const step: StepDefinition = createRespondToClaimFormStep({
 
     const paymentReference = req.session.payment?.paymentReference;
     const t = getTranslationFunction(req);
+    const caseId = req.res?.locals.validatedCase?.id;
+
+    if (isLegalRepresentativeUser(req)) {
+      const caseDetailsBaseUrl = config.has('redirects.manageCaseReturnURL')
+        ? config.get<string>('redirects.manageCaseReturnURL')
+        : null;
+      redirectUrl = buildManageCaseDetailsRedirect(caseDetailsBaseUrl, caseId);
+    } else {
+      redirectUrl = getDashboardUrl(caseId);
+    }
 
     return {
       paymentReferenceLine: paymentReference ? t('paymentReference', { paymentReference }) : undefined,
+      redirectUrl
     };
   },
 });
