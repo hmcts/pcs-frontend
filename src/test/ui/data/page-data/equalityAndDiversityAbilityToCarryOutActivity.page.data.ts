bbf15590181779b83e064e5f26ac1707d78519f7
ankita <ankita.kumbhare@hmcts.net>
2026-08-15 00:08:07 +0100

    Adding scenarios for PCA

---
+++ src/test/ui/data/page-data/equalityAndDiversityAbilityToCarryOutActivity.page.data.ts
@@ -0,0 +1,9 @@
+export const equalityAndDiversityAbilityToCarryOutActivity = {
+  mainHeader: `Do any of your conditions or illnesses reduce your ability to carry out day-to-day activities?`,
+  yesALotRadioOption: `Yes, limited a lot`,
+  yesALittleRadioOption: `Yes, limited a little`,
+  notAtAllRadioOption: `Not at all`,
+  preferNotToSayRadioOption: `Prefer not to say `,
+  ContinueButton: `Continue`,
+  backLink: `Back`,
+};
