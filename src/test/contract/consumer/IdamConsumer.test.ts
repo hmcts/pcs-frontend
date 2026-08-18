a4c3268933d0b7be8e94cc504112c51b487ea509
jessieharrigan <144362161+jessieharrigan@users.noreply.github.com>
2026-08-18 14:47:33 +0100

    Upgrade to V4

--- src/test/contract/consumer/IdamConsumer.test.ts
+++ src/test/contract/consumer/IdamConsumer.test.ts
@@ -1,9 +1,9 @@
-import { MatchersV3, PactV3 } from '@pact-foundation/pact/src/v3';
+import { MatchersV3, PactV4 } from '@pact-foundation/pact';
 import axios from 'axios';
 
 const { like } = MatchersV3;
 
-const mockProvider = new PactV3({
+const mockProvider = new PactV4({
   consumer: 'pcs_frontend',
   provider: 'idamApi_oidc',
   dir: './pact/pacts',
@@ -21,38 +21,34 @@ describe('Idam Consumer Pact Test', () => {
       roles: ['caseworker'],
     };
 
-    mockProvider.addInteraction({
-      states: [{ description: 'userinfo is requested' }],
-      uponReceiving: 'a request to get user details',
-      withRequest: {
-        method: 'GET',
-        path: '/o/userinfo',
-        headers: {
+    await mockProvider
+      .addInteraction()
+      .given('userinfo is requested')
+      .uponReceiving('a request to get user details')
+      .withRequest('GET', '/o/userinfo', builder => {
+        builder.headers({
           Authorization: ACCESS_TOKEN,
-        },
-      },
-      willRespondWith: {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: like(expectedUserInfo),
-      },
-    });
+        });
+      })
+      .willRespondWith(200, builder => {
+        builder.headers({ 'Content-Type': 'application/json' });
+        builder.jsonBody(like(expectedUserInfo));
+      })
+      .executeTest(async mockserver => {
+        const response = await axios.get(`${mockserver.url}/o/userinfo`, {
+          headers: {
+            Authorization: ACCESS_TOKEN,
+          },
+        });
 
-    await mockProvider.executeTest(async mockserver => {
-      const response = await axios.get(`${mockserver.url}/o/userinfo`, {
-        headers: {
-          Authorization: ACCESS_TOKEN,
-        },
+        expect(response.status).toBe(200);
+        expect(response.data).toEqual(expectedUserInfo);
       });
-
-      expect(response.status).toBe(200);
-      expect(response.data).toEqual(expectedUserInfo);
-    });
   });
 
   test('should receive access token from /o/token', async () => {
     const formData = {
-      client_id: 'pcs_frontend',
+      client_id: 'pt_frontend',
       client_secret: 'AAAAAA',
       grant_type: 'authorization_code',
       redirect_uri: 'http://someRedirectURL',
@@ -63,89 +59,79 @@ describe('Idam Consumer Pact Test', () => {
 
     const formBodyString = new URLSearchParams(formData).toString();
 
-    mockProvider.addInteraction({
-      states: [{ description: 'a token is requested' }],
-      uponReceiving: 'a request to get the access token',
-      withRequest: {
-        method: 'POST',
-        path: '/o/token',
-        headers: {
+    await mockProvider
+      .addInteraction()
+      .given('a token is requested')
+      .uponReceiving('a request to get the access token')
+      .withRequest('POST', '/o/token', builder => {
+        builder.headers({
           Accept: 'application/json',
           'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: formBodyString,
-      },
-      willRespondWith: {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: {
+        });
+        builder.body('text/plain', Buffer.from(formBodyString));
+      })
+      .willRespondWith(200, builder => {
+        builder.headers({ 'Content-Type': 'application/json' });
+        builder.jsonBody({
           access_token: like(ACCESS_TOKEN),
-        },
-      },
-    });
+        });
+      })
+      .executeTest(async mockserver => {
+        const response = await axios.post(`${mockserver.url}/o/token`, formBodyString, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        });
 
-    await mockProvider.executeTest(async mockserver => {
-      const response = await axios.post(`${mockserver.url}/o/token`, formBodyString, {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
+        expect(response.status).toBe(200);
+        expect(response.data).toEqual({ access_token: ACCESS_TOKEN });
       });
-
-      expect(response.status).toBe(200);
-      expect(response.data).toEqual({
-        access_token: ACCESS_TOKEN,
-      });
-    });
   });
+});
 
-  test('a request made to a .well-known endpoint', async () => {
-    const expectedResponse = {
-      request_parameter_supported: true,
-      claims_parameter_supported: false,
-      scopes_supported: ['openid'],
-      issuer: 'https://idam-web-public.aat.platform.hmcts.net/o',
-      id_token_encryption_enc_values_supported: ['A256GCM'],
-      acr_values_supported: [],
-      authorization_endpoint: 'https://idam-web-public.aat.platform.hmcts.net/o/authorize',
-      request_object_encryption_enc_values_supported: ['A256GCM'],
-      rcs_request_encryption_alg_values_supported: ['RSA-OAEP'],
-      claims_supported: [],
-      rcs_request_signing_alg_values_supported: ['PS384'],
-      token_endpoint_auth_methods_supported: ['client_secret_post'],
-      token_endpoint: 'https://idam-web-public.aat.platform.hmcts.net/o/token',
-      response_types_supported: ['code'],
-      request_uri_parameter_supported: true,
-      rcs_response_encryption_enc_values_supported: ['A256GCM'],
-      end_session_endpoint: 'https://idam-web-public.aat.platform.hmcts.net/o/endSession',
-      rcs_request_encryption_enc_values_supported: ['A256GCM'],
-      version: '3.0',
-      rcs_response_encryption_alg_values_supported: ['RSA-OAEP'],
-      userinfo_endpoint: 'https://idam-web-public.aat.platform.hmcts.net/o/userinfo',
-      id_token_encryption_alg_values_supported: ['RSA-OAEP'],
-      jwks_uri: 'https://idam-web-public.aat.platform.hmcts.net/o/jwks',
-      subject_types_supported: ['public'],
-      id_token_signing_alg_values_supported: ['ES384'],
-      request_object_signing_alg_values_supported: ['ES384'],
-      request_object_encryption_alg_values_supported: ['RSA-OAEP'],
-      rcs_response_signing_alg_values_supported: ['PS384'],
-    };
+test('a request made to a .well-known endpoint', async () => {
+  const expectedResponse = {
+    request_parameter_supported: true,
+    claims_parameter_supported: false,
+    scopes_supported: ['openid'],
+    issuer: 'https://idam-web-public.aat.platform.hmcts.net/o',
+    id_token_encryption_enc_values_supported: ['A256GCM'],
+    acr_values_supported: [],
+    authorization_endpoint: 'https://idam-web-public.aat.platform.hmcts.net/o/authorize',
+    request_object_encryption_enc_values_supported: ['A256GCM'],
+    rcs_request_encryption_alg_values_supported: ['RSA-OAEP'],
+    claims_supported: [],
+    rcs_request_signing_alg_values_supported: ['PS384'],
+    token_endpoint_auth_methods_supported: ['client_secret_post'],
+    token_endpoint: 'https://idam-web-public.aat.platform.hmcts.net/o/token',
+    response_types_supported: ['code'],
+    request_uri_parameter_supported: true,
+    rcs_response_encryption_enc_values_supported: ['A256GCM'],
+    end_session_endpoint: 'https://idam-web-public.aat.platform.hmcts.net/o/endSession',
+    rcs_request_encryption_enc_values_supported: ['A256GCM'],
+    version: '3.0',
+    rcs_response_encryption_alg_values_supported: ['RSA-OAEP'],
+    userinfo_endpoint: 'https://idam-web-public.aat.platform.hmcts.net/o/userinfo',
+    id_token_encryption_alg_values_supported: ['RSA-OAEP'],
+    jwks_uri: 'https://idam-web-public.aat.platform.hmcts.net/o/jwks',
+    subject_types_supported: ['public'],
+    id_token_signing_alg_values_supported: ['ES384'],
+    request_object_signing_alg_values_supported: ['ES384'],
+    request_object_encryption_alg_values_supported: ['RSA-OAEP'],
+    rcs_response_signing_alg_values_supported: ['PS384'],
+  };
 
-    mockProvider.addInteraction({
-      states: [{ description: '.well-known endpoint' }],
-      uponReceiving: 'a request for configuration',
-      withRequest: {
-        method: 'GET',
-        path: '/o/.well-known/openid-configuration',
-      },
-      willRespondWith: {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: like(expectedResponse),
-      },
-    });
-
-    await mockProvider.executeTest(async mockserver => {
+  await mockProvider
+    .addInteraction()
+    .given('.well-known endpoint')
+    .uponReceiving('a request for configuration')
+    .withRequest('GET', '/o/.well-known/openid-configuration')
+    .willRespondWith(200, builder => {
+      builder.headers({ 'Content-Type': 'application/json' });
+      builder.jsonBody(like(expectedResponse));
+    })
+    .executeTest(async mockserver => {
       const response = await axios.get(`${mockserver.url}/o/.well-known/openid-configuration`, {
         headers: {
           Accept: 'application/json',
@@ -156,5 +142,4 @@ describe('Idam Consumer Pact Test', () => {
       expect(response.status).toBe(200);
       expect(response.data).toEqual(expectedResponse);
     });
-  });
 });
