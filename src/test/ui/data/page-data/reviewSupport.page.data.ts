8986eac0819fee2ca7439805ae90b605f53c2cf6
toby-plunkett <toby.plunkett@hmcts.net>
2026-08-19 16:16:59 +0100

    Revert "Hdpi 7379 c UI your support ra (#1917)"
    This reverts commit 31f5f62be950cde3fe58f74004b078121053ffeb.

--- src/test/ui/data/page-data/reviewSupport.page.data.ts
+++
@@ -1,7 +0,0 @@
-export const reviewSupport = {
-  mainHeader: `Review the support you've requested`,
-  backLink: `Back`,
-  submitButton: `Submit`,
-  cancelLink: `Cancel`,
-  newSupportRequestButton: `Add a new support request`,
-};
