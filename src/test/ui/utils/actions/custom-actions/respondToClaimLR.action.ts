98a0e4c406d789d9f09ec32fcff7bde648b7dbbf
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-08-27 10:11:47 +0100

    fix timeout on CYA

--- src/test/ui/utils/actions/custom-actions/respondToClaimLR.action.ts
+++ src/test/ui/utils/actions/custom-actions/respondToClaimLR.action.ts
@@ -111,7 +111,7 @@ export class RespondToClaimLRAction extends RespondToClaimAction implements IAct
       ['confirmDefendantDetailsLR', () => this.confirmDefendantDetailsLR(fieldName as actionRecord)],
       ['enterDateOfBirthDetailsLR', () => this.enterDateOfBirthDetailsLR(fieldName as actionRecord)],
       ['languageUsedLR', () => this.languageUsedLR(fieldName as actionRecord)],
-      ['selectStatementOfTruthRTCLR', () => this.selectStatementOfTruthRTCLR(fieldName as actionRecord)],
+      ['selectStatementOfTruthRTCLR', () => this.selectStatementOfTruthRTCLR(page, fieldName as actionRecord)],
       ['emailConfirmationLR', () => this.emailConfirmationLR(fieldName as actionRecord)],
       ['exemptLandlordLR', () => this.exemptLandlordLR(fieldName as actionRecord)],
       ['selectWrittenTermsLR', () => this.selectWrittenTermsLR(fieldName as actionRecord)],
@@ -1026,7 +1026,7 @@ export class RespondToClaimLRAction extends RespondToClaimAction implements IAct
     await performAction('clickButton', languageUsed.saveAndContinueButton);
   }
 
-  private async selectStatementOfTruthRTCLR(sot: actionRecord): Promise<void> {
+  private async selectStatementOfTruthRTCLR(page: Page, sot: actionRecord): Promise<void> {
     await performValidation('elementToBeVisible', endOfJourneyCYA.contemptOfCourtParagraph);
     await performAction('check', sot.checkBox);
     await performAction('inputText', endOfJourneyCYA.fullNameTextLabel, sot.firstName);
@@ -1034,6 +1034,11 @@ export class RespondToClaimLRAction extends RespondToClaimAction implements IAct
     await performAction('inputText', endOfJourneyCYA.positionOrOfficeHeldTextLabel, sot.position);
 
     await performAction('clickButton', endOfJourneyCYA.submitButton);
+    await page.locator('h1.govuk-panel__title').waitFor({
+      state: 'visible',
+
+      timeout: 30000,
+    });
   }
 
   private async selectUniversalCreditLR(universalCreditDateData: actionRecord): Promise<void> {
