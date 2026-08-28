0c32366cee8496834268ae65f510b795ab2dd5f8
Gautham059 <gautham.bhikkumalla@hmcts.net>
2026-08-28 10:39:33 +0100

    Workers reduced

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: 2,
+  workers: 1,
   timeout: 600 * 1000,
   expect: { timeout: 10 * 1000 },
   use: { actionTimeout: 10 * 1000, navigationTimeout: 30 * 1000 },
