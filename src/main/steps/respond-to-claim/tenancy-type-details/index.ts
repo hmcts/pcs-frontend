a26a1bddf76a30d7d75984ed95269dbe0aeabcd2
Jonathan Ditcher <jonathan.ditcher@solirius.com>
2026-08-28 14:58:41 +0100

    testing iteral

--- src/main/steps/respond-to-claim/tenancy-type-details/index.ts
+++ src/main/steps/respond-to-claim/tenancy-type-details/index.ts
@@ -149,7 +149,7 @@ export const step: StepDefinition = createRespondToClaimFormStep({
       ? caseData?.otherLicenceTypeDetails
       : caseData?.tenancy_DetailsOfOtherTypeOfTenancyLicence;
     // England: tenancy_* (TenancyLicenceDetails).
-    const tenancyTypeAgreementType = TENANCY_TYPE_TO_TEXT[tenancyTypeOfTenancyLicence];
+    const tenancyTypeAgreementType = TENANCY_TYPE_TO_TEXT[tenancyTypeOfTenancyLicence] + 'testtest';
     const senderName = isLegalRepresentativeUser(req) ? claimantName : orgName;
     const release12Enabled = isRelease12Enabled(req);
 
