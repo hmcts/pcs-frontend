f168f06838478f44da81232a45f103bba66b7a70
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-09-02 16:28:21 +0100

    removed temp @regression

--- src/test/ui/e2eTest/respondToAClaimWales.spec.ts
+++ src/test/ui/e2eTest/respondToAClaimWales.spec.ts
@@ -786,7 +786,7 @@ test.describe('Respond to a claim - e2e Journey @nightly', async () => {
 });
 
 test.describe('Common Component YS And PCQ Respond to a claim - e2e Journey @nightly ', async () => {
-  test('Your Support Request Sent to Court And PCQ Respond to a claim - Wales - Secure contract - RentArrears and NonRentArrears - SelectCounterClaim - Yes - CounterClaimFee - INeedHelp @nightly temp - @regression', async () => {
+  test('Your Support Request Sent to Court And PCQ Respond to a claim - Wales - Secure contract - RentArrears and NonRentArrears - SelectCounterClaim - Yes - CounterClaimFee - INeedHelp @nightly', async () => {
     //Single named party - A sum of money or comp - specific sum of money (Yes) - counterclaimFee- I need help
     await performAction('selectLegalAdvice', freeLegalAdvice.yesRadioOption);
     await performAction('selectDoYouHaveASolicitor', doYouHaveASolicitor.noRadioOption);
