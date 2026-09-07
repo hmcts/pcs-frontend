4921932f9b3c471be9cfac807213fba75c1b1ef7
ashajayaprakash <asha.jayaprakash@hmcts.net>
2026-09-07 18:26:14 +0100

    removed @healthCheck

--- src/test/ui/e2eTest/groupAccessLR.spec.ts
+++ src/test/ui/e2eTest/groupAccessLR.spec.ts
@@ -241,7 +241,7 @@ test.describe('Legal representative organisation access after Notice of Change @
     await performValidation('radioButtonChecked', defendantNameConfirmation.noRadioOption, false);
   });
 
-  test('Citizen draft is deleted when Notice of Change gives access to the legal representative organisation @LR @citizenDraftRevocation @healthCheck', async ({
+  test('Citizen draft is deleted when Notice of Change gives access to the legal representative organisation @LR @citizenDraftRevocation', async ({
     page,
     context,
   }) => {
